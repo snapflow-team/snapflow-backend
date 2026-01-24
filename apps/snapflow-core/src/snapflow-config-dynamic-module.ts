2e0c50099cd82c9abcac42575cb7bf25037c3cfe
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-01-24 15:04:49 +0300

    feat(core): добавить комментарий для улучшения настройки envFilePath
    - Указать, что текущий путь в envFilePath дублируется и предложить оптимизацию через envFilePaths

--- apps/snapflow-core/src/snapflow-config-dynamic-module.ts
+++ apps/snapflow-core/src/snapflow-config-dynamic-module.ts
@@ -3,6 +3,8 @@ import { join } from 'path';
 
 // you must import this const in the head of your app.module.ts
 export const snapFlowConfigDynamicModule = ConfigModule.forRoot({
+  //todo: зачем дублировать envFilePath? Если он лежит в файле "env-file-paths.ts"
+  //почему не сделать: envFilePaths: envFilePaths?
   envFilePath: [
     process.env.ENV_FILE_PATH?.trim() || '',
     join(process.cwd(), 'env', `.env.${process.env.NODE_ENV}.local`),
