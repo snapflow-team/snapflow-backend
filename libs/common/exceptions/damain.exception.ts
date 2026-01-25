7a29ee46af8600e234afc9d7bdf3160987ac049c
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-01-25 21:03:59 +0300

    refactor...

--- libs/common/exceptions/damain.exception.ts
+++ libs/common/exceptions/damain.exception.ts
@@ -11,7 +11,3 @@ export class DomainException extends HttpException {
     super({ code, message, errors }, status);
   }
 }
-
-// if (!user) {
-//   throw new UserNotFoundException(userId); // Фильтр вернет 404 и code: "USER_NOT_FOUND"
-// }
