3466b46b9a1f9bd38c7c969b38dae77cf595940f
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-02-02 20:26:10 +0300

    feat(users): добавить сервис для валидации уникальности пользователя
    - Создать `UserValidationService` в modules/user-accounts/users/application/services для проверки уникальности пользователя
    - Добавить метод `validateUniqueUser` для проверки наличия дубликатов по username и email
    - Использовать `UsersRepository` для поиска существующих записей в базе данных
    - Вернуть массив ошибок с деталями полей (username, email), если обнаружены конфликты
    - Бросать исключение `ValidationException` при обнаружении ошибок для унифицированной обработки ошибок валидации
    - Улучшить логику валидации перед созданием нового пользователя, предотвращая дублирование данных

---
+++ apps/snapflow-core/src/modules/user-accounts/users/application/services/user-validation.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@nestjs/common';
+import { UsersRepository } from '../../infrastructure/users.repository';
+import { CryptoService } from '../../../../../../../../libs/common/services/crypto.service';
+import { ValidationErrorDetail } from '../../../../../../../../libs/common/exceptions/interfaces/validation-error-detail.interface';
+import { ValidationException } from '../../../../../../../../libs/common/exceptions/validation.exception';
+
+@Injectable()
+export class UserValidationService {
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private readonly cryptoService: CryptoService,
+  ) {}
+
+  async validateUniqueUser(username: string, email: string): Promise<void> {
+    const errors: ValidationErrorDetail[] = [];
+
+    const [byUsername, byEmail] = await Promise.all([
+      this.usersRepository.findByUsername(username),
+      this.usersRepository.findByEmail(email),
+    ]);
+
+    if (byUsername) {
+      errors.push({
+        field: 'username',
+        message: 'User with the same username already exists',
+      });
+    }
+
+    if (byEmail) {
+      errors.push({
+        field: 'email',
+        message: 'User with the same email already exists',
+      });
+    }
+
+    if (errors.length) {
+      throw new ValidationException(errors);
+    }
+  }
+
+  // async authenticateUser(loginOrEmail: string, password: string): Promise<UserContextDto> {
+  //   let user: User | null = await this.usersRepository.getByEmail(loginOrEmail);
+  //
+  //   if (!user) {
+  //     user = await this.usersRepository.getByLogin(loginOrEmail);
+  //   }
+  //
+  //   if (!user) {
+  //     throw new DomainException({
+  //       code: DomainExceptionCode.Unauthorized,
+  //       message: 'Invalid username or password',
+  //     });
+  //   }
+  //
+  //   const isPasswordValid: boolean = await this.cryptoService.comparePassword({
+  //     password,
+  //     hash: user.passwordHash,
+  //   });
+  //
+  //   if (!isPasswordValid) {
+  //     throw new DomainException({
+  //       code: DomainExceptionCode.Unauthorized,
+  //       message: 'Invalid username or password',
+  //     });
+  //   }
+  //
+  //   return { id: user.id };
+  // }
+}
