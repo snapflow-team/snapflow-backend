ae3c244a49d467b29d7ed4015029146572605161
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-02-03 23:55:31 +0300

    feat(test): добавить метод регистрации пользователей в AuthTestManager
    - Создать метод `registration` в классе `AuthTestManager` для массовой регистрации пользователей
    - Добавить параметр `inputDtos` для передачи кастомных данных, иначе использовать генерацию через `TestDtoFactory`
    - Упростить тестирование массовой регистрации через автоматизацию в `AuthTestManager`

--- apps/snapflow-core/test/managers/auth.test-manager.ts
+++ apps/snapflow-core/test/managers/auth.test-manager.ts
@@ -1,8 +1,37 @@
 import { PrismaService } from '../../src/database/prisma.service';
 import { User } from '../../generated/prisma';
+import request, { Response } from 'supertest';
+import { GLOBAL_PREFIX } from '../../../../libs/common/constants/global-prefix.constant';
+import { HttpStatus } from '@nestjs/common';
+import { Server } from 'http';
+import { TestDtoFactory } from '../helpers/test.dto-factory';
+import {
+  RegistrationUserInputDto
+} from '../../src/modules/user-accounts/auth/api/input-dto/registration-user.input-dto';
 
 export class AuthTestManager {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly server: Server,
+  ) {}
+
+  async registration(inputDtos: RegistrationUserInputDto[] = [], count: number = 1): Promise<void> {
+    const dtos: RegistrationUserInputDto[] =
+      inputDtos.length > 0 ? inputDtos : TestDtoFactory.generateRegistrationUserInputDto(count);
+
+    const registrationPromises: Promise<Response>[] = [];
+
+    for (let i = 0; i < dtos.length; i++) {
+      registrationPromises.push(
+        request(this.server)
+          .post(`/${GLOBAL_PREFIX}/auth/registration`)
+          .send(dtos[i])
+          .expect(HttpStatus.NO_CONTENT),
+      );
+    }
+
+    await Promise.all(registrationPromises);
+  }
 
   async getAll(): Promise<User[]> {
     return this.prisma.user.findMany();
