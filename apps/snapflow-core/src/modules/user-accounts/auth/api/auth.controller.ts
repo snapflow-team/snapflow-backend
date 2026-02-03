245476a7a424ac515d209cb8156035292f5c37af
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-02-03 22:17:06 +0300

    feat(auth): вернуть статус 204 при успешной регистрации
    - Добавить декоратор `@HttpCode(HttpStatus.NO_CONTENT)` в метод `registration` AuthController
    - Установить явный статус ответа 204 (No Content) при успешной регистрации пользователя
    - Сделать API более интуитивным: 204 соответствует отсутствию тела ответа при успешной операции
    - Упростить обработку успешных регистраций на стороне клиента через явный HTTP статус

--- apps/snapflow-core/src/modules/user-accounts/auth/api/auth.controller.ts
+++ apps/snapflow-core/src/modules/user-accounts/auth/api/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { RegistrationUserInputDto } from './input-dto/registration-user.input-dto';
 import { CommandBus } from '@nestjs/cqrs';
 import { RegisterUserCommand } from '../application/usecases/register-user.useсase';
@@ -7,6 +7,7 @@ import { RegisterUserCommand } from '../application/usecases/register-user.useс
 export class AuthController {
   constructor(private readonly commandBus: CommandBus) {}
   @Post('registration')
+  @HttpCode(HttpStatus.NO_CONTENT)
   async registration(@Body() body: RegistrationUserInputDto): Promise<void> {
     await this.commandBus.execute(new RegisterUserCommand(body));
   }
