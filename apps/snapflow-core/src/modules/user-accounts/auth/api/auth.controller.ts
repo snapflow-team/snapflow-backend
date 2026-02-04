357b1dec22c134bc94d417d9c21a29c5a0c06a23
Sabir <magsabir95@gmail.com>
2026-02-04 21:55:58 +0300

    feat(auth): добавить подтверждение регистрации по email
    Добавлен POST /auth/registration-confirmation
    для подтверждения регистрации пользователя по коду из письма

--- apps/snapflow-core/src/modules/user-accounts/auth/api/auth.controller.ts
+++ apps/snapflow-core/src/modules/user-accounts/auth/api/auth.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
-import { RegistrationUserInputDto } from './input-dto/registration-user.input-dto';
 import { CommandBus } from '@nestjs/cqrs';
+import { RegistrationUserInputDto } from './input-dto/registration-user.input-dto';
 import { RegisterUserCommand } from '../application/usecases/register-user.useсase';
+import { ConfirmationEmailCodeInputDto } from './input-dto/confirmation-email-code.input-dto';
+import { ConfirmationEmailCommand } from '../application/usecases/confirmation-email.usecase';
 
 @Controller('auth')
 export class AuthController {
@@ -10,5 +12,11 @@ export class AuthController {
   @HttpCode(HttpStatus.NO_CONTENT)
   async registration(@Body() body: RegistrationUserInputDto): Promise<void> {
     await this.commandBus.execute(new RegisterUserCommand(body));
+  }
+
+  @Post('registration-confirmation')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async confirmRegistration(@Body() body: ConfirmationEmailCodeInputDto): Promise<void> {
+    await this.commandBus.execute(new ConfirmationEmailCommand(body.code));
   }
 }
