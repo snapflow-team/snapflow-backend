8301aef78f2c81a6472423f4fc7e2186a76e7ce8
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-02-05 16:28:43 +0300

    feat(auth): добавить endpoint для логина пользователей
    - Реализовать метод login в AuthController с использованием LocalAuthGuard
    - Внедрить LocalAuthGuard для проверки учетных данных пользователя
    - Использовать декораторы ExtractUserFromRequest и ExtractClientInfo для извлечения данных пользователя и клиента
    - Выполнить команду LoginUserCommand через CommandBus для генерации токенов
    - Добавить установку cookie с refresh токеном через метод res.cookie
    - Вернуть access токен в формате LoginViewDto
    - Повысить безопасность и удобство аутентификации через обработку токенов и cookie

--- apps/snapflow-core/src/modules/user-accounts/auth/api/auth.controller.ts
+++ apps/snapflow-core/src/modules/user-accounts/auth/api/auth.controller.ts
@@ -1,18 +1,52 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Res, UseGuards } from '@nestjs/common';
 import { RegistrationUserInputDto } from './input-dto/registration-user.input-dto';
 import { CommandBus } from '@nestjs/cqrs';
 import { RegisterUserCommand } from '../application/usecases/register-user.useсase';
 import { ApiTags } from '@nestjs/swagger';
 import { ApiRegistration } from './swagger/registration.swagger';
+import { LocalAuthGuard } from '../domain/guards/local/local-auth.guard';
+import { ExtractUserFromRequest } from '../domain/guards/decorators/extract-user-from-request.decorator';
+import { UserContextDto } from '../domain/guards/dto/user-context.dto';
+import { ExtractClientInfo } from '../../../../../../../libs/common/decorators/request/extract-client-info.decorator';
+import { ClientInfoDto } from '../../../../../../../libs/common/dto/client-info.dto';
+import { LoginViewDto } from './view-dto/login.view-dto';
+import { AuthTokens } from '../domain/types/auth-tokens.type';
+import { LoginUserCommand } from '../application/usecases/login-user.usecase';
+import type { Response } from 'express';
+import { UserAccountsConfig } from '../../config/user-accounts.config';
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly commandBus: CommandBus) {}
+  constructor(
+    private readonly userAccountsConfig: UserAccountsConfig,
+    private readonly commandBus: CommandBus,
+  ) {}
   @Post('registration')
   @HttpCode(HttpStatus.NO_CONTENT)
   @ApiRegistration()
   async registration(@Body() body: RegistrationUserInputDto): Promise<void> {
     await this.commandBus.execute(new RegisterUserCommand(body));
+  }
+
+  @Post('login')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @UseGuards(LocalAuthGuard)
+  async login(
+    @ExtractUserFromRequest() user: UserContextDto,
+    @ExtractClientInfo() clientInfo: ClientInfoDto,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<LoginViewDto> {
+    const { accessToken, refreshToken }: AuthTokens = await this.commandBus.execute(
+      new LoginUserCommand({
+        userId: user.id,
+        ip: clientInfo.ip,
+        userAgent: clientInfo.userAgent,
+      }),
+    );
+
+    res.cookie('refreshToken', refreshToken, this.userAccountsConfig.getCookieConfig());
+
+    return { accessToken };
   }
 }
