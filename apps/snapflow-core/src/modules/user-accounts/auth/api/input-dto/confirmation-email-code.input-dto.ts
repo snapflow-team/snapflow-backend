d900dae9ff0e5929644b2f83c16024884dc81432
Sabir <magsabir95@gmail.com>
2026-02-05 23:29:24 +0300

    docs(auth): заменить пример confirmation code на валидный UUID в Swagger
    Обновлён example в ConfirmationEmailCodeInputDto
    для соответствия формату UUID v4

--- apps/snapflow-core/src/modules/user-accounts/auth/api/input-dto/confirmation-email-code.input-dto.ts
+++ apps/snapflow-core/src/modules/user-accounts/auth/api/input-dto/confirmation-email-code.input-dto.ts
@@ -3,6 +3,10 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class ConfirmationEmailCodeInputDto {
   @IsUUID('4', { message: 'Invalid confirmation code' })
-  @ApiProperty({ example: 'string', description: 'confirmation code', type: 'string' })
+  @ApiProperty({
+    example: 'f11cf4ee-2e4e-433b-8539-0634d79e8db',
+    description: 'confirmation code',
+    type: 'string',
+  })
   code: string;
 }
