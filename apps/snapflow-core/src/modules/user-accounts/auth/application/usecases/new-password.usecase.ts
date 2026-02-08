db7dee3bd0f3719e18f6af7972a771b0a673f841
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-02-08 12:08:47 +0300

    feat(auth): реализовать логику смены пароля в NewPasswordUseCase
    - Добавить метод execute в NewPasswordUseCase для обработки смены пароля
    - Импортировать UsersRepository, CryptoService и DateService для работы с данными пользователя, хэшированием пароля и проверкой даты
    - Проверять наличие пользователя и кода восстановления через метод findUserByPasswordRecoveryCode
    - Добавить проверку на истечение срока действия recoveryCode с использованием DateService
    - Реализовать хэширование нового пароля через CryptoService.createPasswordHash
    - Обновить пароль пользователя и сбросить recoveryCode методом updatePasswordAndResetRecoveryCode
    - Повысить безопасность и функциональность модуля смены пароля через улучшенную обработку кода восстановления

--- apps/snapflow-core/src/modules/user-accounts/auth/application/usecases/new-password.usecase.ts
+++ apps/snapflow-core/src/modules/user-accounts/auth/application/usecases/new-password.usecase.ts
@@ -1,4 +1,11 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { UsersRepository } from '../../../users/infrastructure/users.repository';
+import { CryptoService } from '../../../../../../../../libs/common/services/crypto.service';
+import { UserWithPasswordRecoveryCode } from '../../../users/types/user-with-password-recovery.type';
+import { DomainException } from '../../../../../../../../libs/common/exceptions/damain.exception';
+import { ErrorCodes } from '../../../../../../../../libs/common/exceptions/error-codes.enum';
+import { HttpStatus } from '@nestjs/common';
+import { DateService } from '../../../../../../../../libs/common/services/date.service';
 
 export class NewPasswordCommand {
   constructor(
@@ -9,5 +16,37 @@ export class NewPasswordCommand {
 
 @CommandHandler(NewPasswordCommand)
 export class NewPasswordUseCase implements ICommandHandler<NewPasswordCommand> {
-  async execute(command: NewPasswordCommand): Promise<void> {}
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private readonly cryptoService: CryptoService,
+    private readonly dateService: DateService,
+  ) {}
+
+  async execute({ newPassword, recoveryCode }: NewPasswordCommand): Promise<void> {
+    const user: UserWithPasswordRecoveryCode | null =
+      await this.usersRepository.findUserByPasswordRecoveryCode(recoveryCode);
+
+    if (!user || !user.passwordRecoveryCode) {
+      throw new DomainException(
+        ErrorCodes.BAD_REQUEST,
+        'Recovery code is invalid or expired',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    if (
+      user.passwordRecoveryCode.expirationDate &&
+      this.dateService.isExpired(user.passwordRecoveryCode.expirationDate)
+    ) {
+      throw new DomainException(
+        ErrorCodes.BAD_REQUEST,
+        'Recovery code has expired',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const passwordHash: string = await this.cryptoService.createPasswordHash(newPassword);
+
+    await this.usersRepository.updatePasswordAndResetRecoveryCode(user.id, passwordHash);
+  }
 }
