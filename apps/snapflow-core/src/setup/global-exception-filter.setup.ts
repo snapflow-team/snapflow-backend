8c8917de532ad20e7d8424482485a70778871563
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-02-09 10:54:50 +0300

    feat(common): добавить фильтр GlobalHttpExceptionsFilter
    - Создать фильтр GlobalHttpExceptionsFilter для обработки исключений в контексте HTTP
    - Реализовать обработку HttpException и внутренних ошибок с использованием ErrorResponseDto
    - Поддержать настройку sendInternalServerErrorDetails для управления видимостью деталей ошибок
    - Заменить устаревший AllExceptionsFilter на новый GlobalHttpExceptionsFilter в setup
    - Улучшить обработку исключений и повысить безопасность за счёт гибкой настройки отправки ошибок клиенту

--- apps/snapflow-core/src/setup/global-exception-filter.setup.ts
+++ apps/snapflow-core/src/setup/global-exception-filter.setup.ts
@@ -1,7 +1,7 @@
 import { INestApplication } from '@nestjs/common';
 import { SnapflowCoreConfig } from '../snapflow-core.config';
-import { AllExceptionsFilter } from '../../../../libs/common/exceptions/filters/all-exceptions.filter';
+import { AllHttpExceptionsFilter } from '../../../../libs/common/exceptions/filters/global-http-exceptions.filter';
 
 export function globalExceptionFilterSetup(app: INestApplication, config: SnapflowCoreConfig) {
-  app.useGlobalFilters(new AllExceptionsFilter(config));
+  app.useGlobalFilters(new AllHttpExceptionsFilter(config));
 }
