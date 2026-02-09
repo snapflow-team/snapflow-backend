1c3ca957dc690d95b2eed8c530e208f2a2a260c6
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-02-09 11:03:05 +0300

    feat(core): обновить настройку globalExceptionFilterSetup
    - Заменить AllHttpExceptionsFilter на новые фильтры: ValidationExceptionFilter, DomainHttpExceptionsFilter и GlobalExceptionsFilter
    - Подключить ValidationExceptionFilter для обработки ошибок валидации
    - Использовать DomainHttpExceptionsFilter для обработки доменных исключений в HTTP контексте
    - Применить GlobalExceptionsFilter для обработки базовых исключений с поддержкой SnapflowCoreConfig
    - Повысить модульность и читаемость обработки исключений в приложении, заменив устаревший фильтр на более гибкое решение

--- apps/snapflow-core/src/setup/global-exception-filter.setup.ts
+++ apps/snapflow-core/src/setup/global-exception-filter.setup.ts
@@ -1,7 +1,13 @@
 import { INestApplication } from '@nestjs/common';
 import { SnapflowCoreConfig } from '../snapflow-core.config';
-import { AllHttpExceptionsFilter } from '../../../../libs/common/exceptions/filters/global-http-exceptions.filter';
+import { GlobalExceptionsFilter } from '../../../../libs/common/exceptions/filters/global-http-exceptions.filter';
+import { ValidationExceptionFilter } from '../../../../libs/common/exceptions/filters/validation-http-exception.filter';
+import { DomainHttpExceptionsFilter } from '../../../../libs/common/exceptions/filters/domain-http-exceptions.filter';
 
 export function globalExceptionFilterSetup(app: INestApplication, config: SnapflowCoreConfig) {
-  app.useGlobalFilters(new AllHttpExceptionsFilter(config));
+  app.useGlobalFilters(
+    new ValidationExceptionFilter(),
+    new DomainHttpExceptionsFilter(),
+    new GlobalExceptionsFilter(config),
+  );
 }
