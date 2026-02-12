f4b7a28922b9fb1bf7f9bc9ad7dede28f150b14a
Sabir <magsabir95@gmail.com>
2026-02-12 22:56:44 +0300

    refactor(auth): заменить прямой вызов JwtService на AuthTokenService в LoginUserUseCase

--- apps/snapflow-core/src/modules/user-accounts/auth/application/usecases/login-user.usecase.ts
+++ apps/snapflow-core/src/modules/user-accounts/auth/application/usecases/login-user.usecase.ts
@@ -1,16 +1,11 @@
 import { CommandBus, CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { Inject } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 import { AuthTokens } from '../../domain/types/auth-tokens.type';
 import { LoginUserApplicationDto } from '../dto/login-user.application-dto';
-import {
-  ACCESS_TOKEN_STRATEGY_INJECT_TOKEN,
-  REFRESH_TOKEN_STRATEGY_INJECT_TOKEN,
-} from '../../constants/auth-tokens.inject-constants';
 import { CryptoService } from '../../../../../../../../libs/common/services/crypto.service';
 import { PayloadRefreshToken } from '../types/payload-refresh-token.type';
 import { CreateSessionDto } from '../../sessions/dto/create-session.dto';
 import { CreateSessionCommand } from '../../sessions/application/usecases/create-session.usecase';
+import { AuthTokenService } from '../../../../../../../../libs/common/services/auth-token.service';
 
 export class LoginUserCommand {
   constructor(public readonly dto: LoginUserApplicationDto) {}
@@ -19,29 +14,19 @@ export class LoginUserCommand {
 @CommandHandler(LoginUserCommand)
 export class LoginUserUseCase implements ICommandHandler<LoginUserCommand> {
   constructor(
-    @Inject(ACCESS_TOKEN_STRATEGY_INJECT_TOKEN)
-    private readonly accessTokenContext: JwtService,
-
-    @Inject(REFRESH_TOKEN_STRATEGY_INJECT_TOKEN)
-    private readonly refreshTokenContext: JwtService,
-
+    private readonly authTokenService: AuthTokenService,
     private readonly cryptoService: CryptoService,
     private readonly commandBus: CommandBus,
   ) {}
 
   async execute({ dto: { userId, ip, userAgent } }: LoginUserCommand): Promise<AuthTokens> {
-    const accessToken: string = this.accessTokenContext.sign({
-      userId,
-    });
+    const accessToken: string = this.authTokenService.generateAccessToken(userId);
 
     const deviceId: string = this.cryptoService.generateUUID();
-    const refreshToken: string = this.refreshTokenContext.sign({
-      userId,
-      deviceId,
-    });
+    const refreshToken: string = this.authTokenService.generateRefreshToken(userId, deviceId);
 
     const { iat, exp }: PayloadRefreshToken =
-      this.refreshTokenContext.decode<PayloadRefreshToken>(refreshToken);
+      this.authTokenService.decodeRefreshToken(refreshToken);
 
     const createSessionDto: CreateSessionDto = {
       userId,
