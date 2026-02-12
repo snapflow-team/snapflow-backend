f53d7b17a16fb849939148bfb460c749a478565a
Sabir <magsabir95@gmail.com>
2026-02-12 22:49:08 +0300

    feat(auth): добавить Google OAuth стратегию через passport-google-oauth20

---
+++ apps/snapflow-core/src/modules/user-accounts/auth/domain/guards/google/google.strategy.ts
@@ -0,0 +1,25 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Profile, Strategy } from 'passport-google-oauth20';
+import { UserAccountsConfig } from '../../../../config/user-accounts.config';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor(private readonly userAccountConfig: UserAccountsConfig) {
+    super({
+      clientID: userAccountConfig.googleClientId,
+      clientSecret: userAccountConfig.googleClientSecret,
+      callbackURL: userAccountConfig.googleCallbackUrl,
+      scope: ['email', 'profile'],
+    });
+  }
+
+  async validate(accessToken: string, refreshToken: string, profile: Profile) {
+    return {
+      provider: 'google',
+      email: profile.emails?.[0].value,
+      name: profile.displayName,
+      providerId: profile.id,
+    };
+  }
+}
