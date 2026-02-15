83f8d332f5a00b8f5a239841122a2255a04a495c
Sabir <magsabir95@gmail.com>
2026-02-15 23:24:05 +0300

    fix(): Заменены старые message на более общие

--- apps/snapflow-core/src/modules/user-accounts/auth/sessions/application/usecases/delete-session-by-device-id.usecase.ts
+++ apps/snapflow-core/src/modules/user-accounts/auth/sessions/application/usecases/delete-session-by-device-id.usecase.ts
@@ -3,6 +3,7 @@ import { SessionsRepository } from '../../infrastructure/sessions.repository';
 import { DomainException } from '../../../../../../../../../libs/common/exceptions/damain.exception';
 import { DomainExceptionCode } from '../../../../../../../../../libs/common/exceptions/types/domain-exception-codes';
 import { SessionContextDto } from '../../../domain/guards/dto/session-context.dto';
+import { Session } from '@generated/prisma';
 
 export class DeleteSessionByDeviceIdCommand {
   constructor(
@@ -21,24 +22,26 @@ export class DeleteSessionByDeviceUseCase
 
     if (deviceId === dto.deviceId) {
       throw new DomainException({
-        code: DomainExceptionCode.Forbidden,
-        message: 'Cannot delete current sessiond',
+        code: DomainExceptionCode.BadRequest,
+        message: 'Cannot terminate the active session you are currently using. Use logout instead.',
+        // «Нельзя завершить текущую сессию через этот запрос. Используйте выход из аккаунта.»
       });
     }
 
-    const session = await this.sessionsRepository.findByDeviceId(deviceId);
+    const session: Session | null = await this.sessionsRepository.findByDeviceId(deviceId);
 
     if (!session) {
       throw new DomainException({
         code: DomainExceptionCode.NotFound,
-        message: 'Session not found',
+        message: 'The specified device session could not be found.',
       });
     }
 
     if (session.userId !== dto.userId) {
       throw new DomainException({
         code: DomainExceptionCode.Forbidden,
-        message: 'Cannot delete this session',
+        message: 'Access denied. You can only manage your own device sessions.',
+        // «Доступ запрещен. Вы можете управлять только своими активными устройствами.»
       });
     }
 
