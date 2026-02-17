909d63dbade1f603beed310d187aac9d6a1c278f
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-02-17 21:28:59 +0300

    feat(auth): реализовать GitHub OAuth стратегию и guard
    - Добавить GithubStrategy с использованием пакета passport-github2 и конфигурации из UserAccountsConfig
    - Реализовать метод validate в GithubStrategy для обработки авторизации и создания объекта GithubContextDto
    - Создать GithubAuthGuard для защиты маршрутов через стратегию 'github'
    - Подготовить инфраструктуру для обработки авторизации через GitHub в системе

---
+++ apps/snapflow-core/src/modules/user-accounts/auth/domain/guards/github/github.strategy.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Profile, Strategy } from 'passport-github2';
+import { UserAccountsConfig } from '../../../../config/user-accounts.config';
+import { OAuthProvider } from '@generated/prisma';
+import { GithubContextDto } from '../dto/github-context.dto';
+
+@Injectable()
+export class GithubStrategy extends PassportStrategy(Strategy, 'github') {
+  constructor(private readonly userAccountsConfig: UserAccountsConfig) {
+    super({
+      clientID: userAccountsConfig.githubOauthClientId,
+      clientSecret: userAccountsConfig.googleClientSecret,
+      callbackURL: userAccountsConfig.githubOauthCallbackUrl,
+      scope: ['user:email'],
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+    done: (err: any, user: any, info?: any) => void,
+  ): Promise<void> {
+    try {
+      const githubContextDto: GithubContextDto = {
+        provider: OAuthProvider.GITHUB,
+        id: profile.id,
+        email: profile.emails?.[0]?.value || null,
+        username: profile.username || null,
+      };
+
+      done(null, githubContextDto);
+    } catch (err) {
+      done(err, null);
+    }
+  }
+}
