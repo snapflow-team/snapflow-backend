c826f5e7f3280f31d60661cfa301f0b5e4ee9ea7
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-02-23 23:03:03 +0300

    TEST DEPLOY 2

--- apps/snapflow-core/src/main.ts
+++ apps/snapflow-core/src/main.ts
@@ -20,7 +20,6 @@ async function bootstrap() {
   const env: string = coreConfig.env;
 
   await app.listen(port, () => {
-    console.log('TEST');
     console.log('App starting listen port: ', port);
     console.log('NODE_ENV: ', env);
   });
