33443a6ea2ff0b9d2b2645245121df84c297f6fe
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-02-23 15:31:46 +0300

    refactor(auth): перенести проверку уникальности пользователя в RegisterUserUseCase
    - Удалить метод validateUniqueUser из UserValidationService
    - Добавить приватный метод isUsernameOrEmailTaken в RegisterUserUseCase для проверки уникальности пользователя
    - Заменить вызов userValidation.validateUniqueUser в RegisterUserUseCase на логику с использованием isUsernameOrEmailTaken
    - Исключить зависимость UserValidationService из RegisterUserUseCase и удалить лишний импорт
    - Упростить код, улучшив читаемость и локализацию логики проверки уникальности пользователей в обработчике команды регистрации

--- apps/snapflow-core/src/modules/user-accounts/auth/application/usecases/register-user.useсase.ts
+++ apps/snapflow-core/src/modules/user-accounts/auth/application/usecases/register-user.useсase.ts
@@ -1,11 +1,11 @@
 import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs';
-import { UserValidationService } from '../../../users/application/services/user-validation.service';
 import { UsersRepository } from '../../../users/infrastructure/users.repository';
 import { RegistrationUserApplicationDto } from '../../../users/application/dto/registration-user.application-dto';
 import { UserRegisteredEvent } from '../../domain/events/user-registered.event';
 import { CryptoService } from '../../../../../../../../libs/common/services/crypto.service';
 import { DateService } from '../../../../../../../../libs/common/services/date.service';
-import { ConfirmationStatus } from '../../../../../../generated/prisma';
+import { ValidationException } from '../../../../../../../../libs/common/exceptions/validation-exception';
+import { ConfirmationStatus, User } from '@generated/prisma';
 
 export class RegisterUserCommand {
   constructor(public readonly dto: RegistrationUserApplicationDto) {}
@@ -14,7 +14,6 @@ export class RegisterUserCommand {
 @CommandHandler(RegisterUserCommand)
 export class RegisterUserUseCase implements ICommandHandler<RegisterUserCommand> {
   constructor(
-    private readonly userValidation: UserValidationService,
     private readonly usersRepository: UsersRepository,
     private readonly cryptoService: CryptoService,
     private readonly dateService: DateService,
@@ -22,7 +21,19 @@ export class RegisterUserUseCase implements ICommandHandler<RegisterUserCommand>
   ) {}
 
   async execute({ dto: { username, email, password } }: RegisterUserCommand): Promise<void> {
-    await this.userValidation.validateUniqueUser(username, email);
+    const check = await this.isUsernameOrEmailTaken(username, email);
+
+    if (check.isTaken) {
+      throw new ValidationException([
+        {
+          field: check.field!,
+          message:
+            check.field === 'username'
+              ? 'User with this username is already registered'
+              : 'User with this email is already registered',
+        },
+      ]);
+    }
 
     const passwordHash: string = await this.cryptoService.createPasswordHash(password);
     const confirmationCode: string = this.cryptoService.generateUUID();
@@ -45,5 +56,22 @@ export class RegisterUserUseCase implements ICommandHandler<RegisterUserCommand>
     });
 
     this.eventBus.publish(new UserRegisteredEvent(email, confirmationCode));
+  }
+
+  private async isUsernameOrEmailTaken(
+    username: string,
+    email: string,
+  ): Promise<{ isTaken: boolean; field?: 'username' | 'email' }> {
+    const userByUsername: User | null = await this.usersRepository.findUserByUsername(username);
+    if (userByUsername) {
+      return { isTaken: true, field: 'username' };
+    }
+
+    const userByEmail: User | null = await this.usersRepository.findUserByEmail(email);
+    if (userByEmail) {
+      return { isTaken: true, field: 'email' };
+    }
+
+    return { isTaken: false };
   }
 }
--- apps/snapflow-core/src/modules/user-accounts/users/application/services/user-validation.service.ts
+++ apps/snapflow-core/src/modules/user-accounts/users/application/services/user-validation.service.ts
@@ -1,11 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { UsersRepository } from '../../infrastructure/users.repository';
-import { CryptoService } from '../../../../../../../../libs/common/services/crypto.service';
-import {
-  DomainException,
-  Extension,
-} from '../../../../../../../../libs/common/exceptions/damain.exception';
-import { ValidationException } from '../../../../../../../../libs/common/exceptions/validation-exception';
+import { DomainException } from '../../../../../../../../libs/common/exceptions/damain.exception';
 import { DomainExceptionCode } from '../../../../../../../../libs/common/exceptions/types/domain-exception-codes';
 import { UserWithPasswordRecoveryCode } from '../../types/user-with-password-recovery.type';
 import { DateService } from '../../../../../../../../libs/common/services/date.service';
@@ -14,37 +9,8 @@ import { DateService } from '../../../../../../../../libs/common/services/date.s
 export class UserValidationService {
   constructor(
     private readonly usersRepository: UsersRepository,
-    private readonly cryptoService: CryptoService,
     private readonly dateService: DateService,
   ) {}
-
-  async validateUniqueUser(username: string, email: string): Promise<void> {
-    const errors: Extension[] = [];
-
-    const [byUsername, byEmail] = await Promise.all([
-      this.usersRepository.findUserByUsername(username),
-      this.usersRepository.findUserByEmail(email),
-    ]);
-
-    if (byUsername) {
-      errors.push({
-        field: 'username',
-        message: 'User with this username is already registered',
-      });
-    }
-
-    if (byEmail) {
-      errors.push({
-        field: 'email',
-        message: 'User with this email is already registered',
-      });
-    }
-
-    if (errors.length) {
-      throw new ValidationException(errors);
-    }
-  }
-
   async validatePasswordRecoveryCode(recoveryCode: string): Promise<UserWithPasswordRecoveryCode> {
     const user: UserWithPasswordRecoveryCode | null =
       await this.usersRepository.findUserByPasswordRecoveryCode(recoveryCode);
