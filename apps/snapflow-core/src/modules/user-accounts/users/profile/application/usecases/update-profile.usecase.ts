b780d3a47d5fffbf95fa406615beebf6e31d9166
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-02-26 23:37:08 +0300

    feat(application): добавить обработчик UpdateProfileUseCase для обновления профиля
    - Реализовать UpdateProfileUseCase с использованием ICommandHandler из NestJS CQRS
    - Проверять минимальный возраст (13 лет) через метод getAge из DateService при обновлении данных профиля
    - Добавить валидацию: вызывать DomainException, если возраст пользователя меньше 13 лет
    - Проверять наличие профиля из ProfilesRepository по userId, выбрасывать DomainException в случае отсутствия
    - Использовать ProfilesRepository для обновления профиля с поддержкой nullable полей
    - Упростить логику обработки команд для обновления пользовательских данных с учетом бизнес-правил и валидации

---
+++ apps/snapflow-core/src/modules/user-accounts/users/profile/application/usecases/update-profile.usecase.ts
@@ -0,0 +1,46 @@
+import { UpdateProfileApplicationDto } from '../dto/update-profile.application-dto';
+import { ICommandHandler } from '@nestjs/cqrs';
+import { UserProfile } from '@generated/prisma';
+import { ProfilesRepository } from '../../infrastructure/profiles.repository';
+import { DomainException } from '../../../../../../../../../libs/common/exceptions/damain.exception';
+import { DomainExceptionCode } from '../../../../../../../../../libs/common/exceptions/types/domain-exception-codes';
+import { DateService } from '../../../../../../../../../libs/common/services/date.service';
+
+export class UpdateProfileCommand {
+  constructor(public readonly dto: UpdateProfileApplicationDto) {}
+}
+
+export class UpdateProfileUseCase implements ICommandHandler<UpdateProfileCommand> {
+  constructor(
+    private readonly profilesRepository: ProfilesRepository,
+    private readonly dateService: DateService,
+  ) {}
+  async execute({ dto: { userId, ...data } }: UpdateProfileCommand) {
+    if (data.dateOfBirth) {
+      const dob = new Date(data.dateOfBirth);
+      const age: number = this.dateService.getAge(dob);
+
+      if (age < 13) {
+        throw new DomainException({
+          code: DomainExceptionCode.BadRequest,
+          message: 'User must be at least 13 years old to update profile',
+        });
+      }
+    }
+
+    const profile: UserProfile | null = await this.profilesRepository.findProfileByUserId(userId);
+
+    if (!profile) {
+      throw new DomainException({
+        code: DomainExceptionCode.InternalServerError,
+        message: 'User profile is missing. Registration invariant violated',
+      });
+    }
+
+    await this.profilesRepository.updateProfile({
+      profileId: profile.id,
+      ...data,
+      dateOfBirth: data.dateOfBirth ? new Date(data.dateOfBirth) : null,
+    });
+  }
+}
