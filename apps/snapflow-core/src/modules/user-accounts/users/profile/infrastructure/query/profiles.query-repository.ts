e759b6e518bfc1f81ec8c12bacc08ce8b2b31e29
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-02-27 22:39:45 +0300

    feat(infrastructure): добавить ProfilesQueryRepository для получения профилей
    - Реализовать класс ProfilesQueryRepository для работы с профилями пользователей
    - Добавить метод findProfileByUserId для получения профиля по userId с учетом soft delete
    - Использовать PrismaService для доступа к данным профилей из базы
    - Выбрасывать DomainException с кодом NotFound, если профиль не найден
    - Упрощён процесс получения профиля через репозиторий: централизованное управление запросами к БД

---
+++ apps/snapflow-core/src/modules/user-accounts/users/profile/infrastructure/query/profiles.query-repository.ts
@@ -0,0 +1,23 @@
+import { PrismaService } from '../../../../../../database/prisma.service';
+import { UserProfile } from '@generated/prisma';
+import { DomainException } from '../../../../../../../../../libs/common/exceptions/damain.exception';
+import { DomainExceptionCode } from '../../../../../../../../../libs/common/exceptions/types/domain-exception-codes';
+import { ProfileViewDto } from '../../api/dto/view-dto/profile.view-dto';
+
+export class ProfilesQueryRepository {
+  constructor(private readonly prisma: PrismaService) {}
+  async findProfileByUserId(userId: number) {
+    const profile: UserProfile | null = await this.prisma.userProfile.findFirst({
+      where: { userId, deletedAt: null },
+    });
+
+    if (!profile) {
+      throw new DomainException({
+        code: DomainExceptionCode.NotFound,
+        message: `The user with the ID (${userId}) does not have a profile`,
+      });
+    }
+
+    return ProfileViewDto.mapToView(profile);
+  }
+}
