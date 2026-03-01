d24a19331c0a4710e7f7beba970a377a58a8d060
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-01 19:53:39 +0300

    feat(core): реализовать систему настроек API
    - Добавить класс ApiSettings для управления настройками API, такими как порты, JWT секреты и параметры OAuth
    - Аннотировать свойства ApiSettings с помощью class-validator для проверки данных из переменных окружения
    - Реализовать методы getJwtOptions, getGoogleOauthOptions, getGithubOauthOptions для получения настроек JWT токенов, Google и Github OAuth
    - Добавить поддержку настроек cookie через метод getCookieOptions с параметрами httpOnly, secure, sameSite и maxAge
    - Добавить класс Configuration для управления загрузкой и валидацией настроек из переменных окружения
    - Использовать dotenv для загрузки переменных окружения в зависимости от NODE_ENV
    - Реализовать метод validate для проверки всех настроек, выбрасывая ошибку при некорректных значениях
    - Упростить работу с настройками через методы createConfig и статическую функцию экспорта конфигурации

---
+++ apps/snapflow-core/src/setup/configuration/api-settings.ts
@@ -0,0 +1,153 @@
+import { IsBoolean, IsEnum, IsNotEmpty, IsNumber, IsString, IsUrl } from 'class-validator';
+import { CookieOptions } from 'express';
+import { EnvironmentVariable } from './configuration';
+
+export enum SameSite {
+  STRICT = 'strict',
+  LAX = 'lax',
+  NONE = 'none',
+}
+
+export class ApiSettings {
+  @IsNumber()
+  port: number;
+
+  @IsNotEmpty()
+  accessTokenSecret: string;
+
+  @IsNotEmpty()
+  refreshTokenSecret: string;
+
+  @IsNotEmpty()
+  accessTokenExpireIn: string | number;
+
+  @IsNotEmpty()
+  refreshTokenExpireIn: string | number;
+
+  @IsNotEmpty()
+  googleClientId: string;
+
+  @IsNotEmpty()
+  googleClientSecret: string;
+
+  @IsUrl({ require_tld: false, protocols: ['http', 'https'] })
+  googleCallbackUrl: string;
+
+  @IsNotEmpty()
+  githubOauthClientId: string;
+
+  @IsNotEmpty()
+  githubOauthClientSecret: string;
+
+  @IsUrl({ require_tld: false, protocols: ['http', 'https'] })
+  githubOauthCallbackUrl: string;
+
+  @IsNotEmpty()
+  googleRecaptchaSecretKey: string;
+
+  @IsBoolean()
+  httpOnly: boolean;
+
+  @IsBoolean()
+  secure: boolean;
+
+  @IsEnum(SameSite)
+  sameSite: SameSite;
+
+  @IsNumber()
+  maxAge: number;
+
+  @IsString()
+  allowedOriginsRaw: string;
+
+  @IsNumber()
+  throttleTtl: number;
+
+  @IsNumber()
+  throttleLimit: number;
+
+  @IsBoolean()
+  isSwaggerEnabled: boolean;
+
+  @IsBoolean()
+  sendInternalServerErrorDetails: boolean;
+
+  constructor(private readonly environmentVariables: EnvironmentVariable) {
+    this.port = Number(environmentVariables.PORT);
+
+    this.accessTokenSecret = environmentVariables.JWT_SECRET_AT;
+    this.refreshTokenSecret = environmentVariables.JWT_SECRET_RT;
+    this.accessTokenExpireIn = environmentVariables.JWT_EXPIRATION_AT;
+    this.refreshTokenExpireIn = environmentVariables.JWT_EXPIRATION_RT;
+
+    this.googleClientId = environmentVariables.GOOGLE_CLIENT_ID;
+    this.googleClientSecret = environmentVariables.GOOGLE_CLIENT_SECRET;
+    this.googleCallbackUrl = environmentVariables.GOOGLE_CALLBACK_URL;
+
+    this.githubOauthClientId = environmentVariables.GITHUB_OAUTH_CLIENT_ID;
+    this.githubOauthClientSecret = environmentVariables.GITHUB_OAUTH_CLIENT_SECRET;
+    this.githubOauthCallbackUrl = environmentVariables.GITHUB_OAUTH_CALLBACK_URL;
+
+    this.googleRecaptchaSecretKey = environmentVariables.GOOGLE_RECAPTCHA_SECRET_KEY;
+
+    this.httpOnly = environmentVariables.HTTP_ONLY === 'true';
+    this.secure = environmentVariables.SECURE === 'true';
+    this.sameSite = environmentVariables.SAME_SITE as SameSite;
+    this.maxAge = Number(environmentVariables.MAX_AGE);
+
+    this.allowedOriginsRaw = environmentVariables.ALLOWED_ORIGINS;
+
+    this.throttleTtl = Number(environmentVariables.THROTTLE_TTL);
+    this.throttleLimit = Number(environmentVariables.THROTTLE_LIMIT);
+
+    this.isSwaggerEnabled = environmentVariables.IS_SWAGGER_ENABLED === 'true';
+
+    this.sendInternalServerErrorDetails =
+      environmentVariables.SEND_INTERNAL_SERVER_ERROR_DETAILS === 'true';
+  }
+
+  getJwtOptions() {
+    return {
+      accessToken: {
+        secret: this.accessTokenSecret,
+        expiresIn: this.accessTokenExpireIn,
+      },
+      refreshToken: {
+        secret: this.refreshTokenSecret,
+        expiresIn: this.refreshTokenExpireIn,
+      },
+    };
+  }
+
+  getGoogleOauthOptions() {
+    return {
+      clientId: this.googleClientId,
+      clientSecret: this.googleClientSecret,
+      callbackURL: this.googleCallbackUrl,
+    };
+  }
+
+  getGithubOauthOptions() {
+    return {
+      clientId: this.githubOauthClientId,
+      clientSecret: this.githubOauthClientSecret,
+      callbackURL: this.githubOauthCallbackUrl,
+    };
+  }
+
+  getCookieOptions(): CookieOptions {
+    return {
+      httpOnly: this.httpOnly,
+      secure: this.secure,
+      sameSite: this.sameSite,
+      maxAge: this.maxAge,
+    };
+  }
+
+  getThrottleOptions() {
+    return {
+      ttl: this.throttleTtl,
+      limit: this.throttleLimit,
+    };
+  }
+}
---
+++ apps/snapflow-core/src/setup/configuration/configuration.ts
@@ -0,0 +1,74 @@
+import { Environments } from '../../../../../libs/common/enums/enviroments.enum';
+import * as dotenv from 'dotenv';
+import { ValidateNested, validateSync } from 'class-validator';
+import { ApiSettings } from './api-settings';
+import { ValidationError } from '@nestjs/common';
+
+export type EnvironmentVariable = { [key: string]: string };
+
+export const loadEnv = (): string[] => {
+  const env = process.env.NODE_ENV as Environments;
+
+  switch (env) {
+    case Environments.Development: {
+      return ['src/env/.env.development.local', 'src/env/.env.development'];
+    }
+
+    case Environments.Testing: {
+      return ['src/env/.env.testing.local', 'src/env/.env.testing'];
+    }
+
+    default: {
+      return ['src/env/.env'];
+    }
+  }
+};
+
+dotenv.config({ path: loadEnv() });
+
+export class Configuration {
+  @ValidateNested()
+  apiSettings: ApiSettings;
+  //
+  //   @ValidateNested()
+  //   databaseSettings: DatabaseSettings;
+  //
+  //   @ValidateNested()
+  //   swaggerSettings: SwaggerSettings;
+  //
+  //   @ValidateNested()
+  //   environmentSettings: EnvironmentSettings;
+  //
+  //   @ValidateNested()
+  //   businessRulesSettings: BusinessRulesSettings;
+  //
+  private constructor(configuration: Configuration) {
+    Object.assign(this, configuration);
+  }
+
+  static createConfig(environmentVariables: EnvironmentVariable): Configuration {
+    return new this({
+      apiSettings: new ApiSettings(environmentVariables),
+      // databaseSettings: new DatabaseSettings(environmentVariables),
+      // swaggerSettings: new SwaggerSettings(environmentVariables),
+      // environmentSettings: new EnvironmentSettings(environmentVariables),
+      // businessRulesSettings: new BusinessRulesSettings(environmentVariables),
+    });
+  }
+}
+
+export function validate(environmentVariables: EnvironmentVariable) {
+  const config: Configuration = Configuration.createConfig(environmentVariables);
+  const errors: ValidationError[] = validateSync(config, { skipMissingProperties: false });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+
+  return config;
+}
+
+export default () => {
+  const environmentVariables = process.env as EnvironmentVariable;
+  return Configuration.createConfig(environmentVariables);
+};
