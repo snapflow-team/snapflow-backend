82f2001d477dea844444c2cb193114de747f1038
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-02 13:29:42 +0300

    refactor(core): упростить настройки инициализации CORS, исключений и Swagger
    - Обновить corsSetup: заменить использование SnapflowCoreConfig на передачу origin напрямую, исключив лишние зависимости
    - Обновить глобальный фильтр исключений: передавать параметр isExposeDetails вместо SnapflowCoreConfig, чтобы сократить зависимости
    - Переработать настройку Swagger: заменить SnapflowCoreConfig на отдельные настройки swaggerSettings и environmentSettings
    - Добавить авторизацию Basic Auth для доступа к Swagger в продакшене, используя express-basic-auth
    - Упростить создание Swagger-документации: улучшить структуру конфигурации, добавить новые теги, активировать deepLinking и другие опции
    - Удалить устаревший избыточный код и комментарии, связанные с продакшен-флагами, для повышения читаемости

--- apps/snapflow-core/src/setup/global-exception-filter.setup.ts
+++ apps/snapflow-core/src/setup/global-exception-filter.setup.ts
@@ -1,11 +1,10 @@
 import { INestApplication } from '@nestjs/common';
-import { SnapflowCoreConfig } from '../snapflow-core.config';
 import { ValidationExceptionFilter } from '../../../../libs/common/exceptions/filters/validation-http-exception.filter';
 import { DomainHttpExceptionsFilter } from '../../../../libs/common/exceptions/filters/domain-http-exceptions.filter';
 import { GlobalExceptionsFilter } from '../../../../libs/common/exceptions/filters/global-http-exceptions.filter';
 
-export function globalExceptionFilterSetup(app: INestApplication, config: SnapflowCoreConfig) {
-  app.useGlobalFilters(new GlobalExceptionsFilter(config));
+export function globalExceptionFilterSetup(app: INestApplication, isExposeDetails: boolean) {
+  app.useGlobalFilters(new GlobalExceptionsFilter(isExposeDetails));
   app.useGlobalFilters(new DomainHttpExceptionsFilter());
   app.useGlobalFilters(new ValidationExceptionFilter());
 }
--- apps/snapflow-core/src/setup/swagger.setup.ts
+++ apps/snapflow-core/src/setup/swagger.setup.ts
@@ -1,18 +1,32 @@
 import { INestApplication } from '@nestjs/common';
-import { SnapflowCoreConfig } from '../snapflow-core.config';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { RequestHandler } from 'express';
+import expressBasicAuth from 'express-basic-auth';
+import { SwaggerSettings } from './configuration/swagger-settings';
+import { EnvironmentSettings } from './configuration/environment-settings';
 import { GLOBAL_PREFIX } from '../../../../libs/common/constants/global-prefix.constant';
 
-export function swaggerSetup(app: INestApplication, config: SnapflowCoreConfig) {
-  //todo: нужно ли скрывать сваггер в проде??
-  // if (config.env === 'production') {
-  //   return;
-  // }
+export function swaggerSetup(
+  app: INestApplication,
+  swaggerSettings: SwaggerSettings,
+  environmentSettings: EnvironmentSettings,
+) {
+  const { swaggerUser, swaggerPassword, swaggerPath } = swaggerSettings;
+  const fullSwaggerPath = `/${GLOBAL_PREFIX}/${swaggerPath}`;
 
-  const swaggerConfig = new DocumentBuilder()
-    .setVersion('1.0')
-    .setTitle('Snapflow API')
-    .setDescription('Документация API для Snapflow.')
+  if (!environmentSettings.isDevelopment) {
+    const authMiddleware: RequestHandler = expressBasicAuth({
+      challenge: true,
+      users: { [swaggerUser]: swaggerPassword },
+    });
+
+    app.use(fullSwaggerPath, authMiddleware);
+  }
+
+  const config: DocumentBuilder = new DocumentBuilder()
+    .setTitle('SnapFlow API')
+    .setVersion('1.0.0')
+    .setDescription('REST API для SnapFlow')
     .addCookieAuth(
       'refreshToken',
       {
@@ -35,22 +49,22 @@ export function swaggerSetup(app: INestApplication, config: SnapflowCoreConfig) 
       'access-token',
     )
     .addTag('Auth', 'Методы аутентификации')
-    .addTag('Users', 'Управление пользователями')
-    .build();
+    .addTag('OAuth', 'Методы аутентификации через сторонние сервисы')
+    .addTag('Profile', 'Управление профилями пользователей')
+    .addTag('Sessions', 'Управление сессиями на разных устройствах');
 
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-
-  SwaggerModule.setup(`${GLOBAL_PREFIX}/docs`, app, document, {
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config.build());
+  SwaggerModule.setup(fullSwaggerPath, app, document, {
     swaggerOptions: {
       persistAuthorization: true,
       filter: true,
       tagsSorter: 'alpha',
       operationsSorter: 'alpha',
+      deepLinking: true,
+      tryItOutEnabled: true,
+      displayOperationId: false,
+      displayRequestDuration: true,
     },
     customSiteTitle: 'Snapflow Documentation',
   });
-
-  if (config.env === 'development') {
-    console.log(`🚀 Swagger is running on: http://localhost:${config.port}/docs`);
-  }
 }
