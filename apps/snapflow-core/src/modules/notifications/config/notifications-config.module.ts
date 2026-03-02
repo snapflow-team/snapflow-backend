85a9919d3597df2b1c06f0e81dac7f7603882e37
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-02 13:28:29 +0300

    refactor(core): удалить модули и настройки, связанные с NotificationsModule
    - Удалить NotificationsModule и связанные файлы: notifications.config, notifications-config.module, notifications.module
    - Удалить SnapflowCoreConfig, appSetup, связанные методы и настройки в snapflow-core.config.ts
    - Упростить кодовую базу, удалив неиспользуемые настройки и логику
    - Облегчить поддержку проекта за счёт удаления избыточных компонентов и конфигураций

--- apps/snapflow-core/src/modules/notifications/config/notifications-config.module.ts
+++
@@ -1,8 +0,0 @@
-import { NotificationsConfig } from './notifications.config';
-import { Module } from '@nestjs/common';
-
-@Module({
-  providers: [NotificationsConfig],
-  exports: [NotificationsConfig],
-})
-export class NotificationsConfigModule {}
