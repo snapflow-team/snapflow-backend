f00e1fe796a2b79931922076d1c45778bb8237f8
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-03 23:55:04 +0300

    feat(files): создать базовую структуру файла, контроллера и модуля
    - Добавить FilesService с методом getHello(), возвращающим строку 'Hello World!'
    - Реализовать FilesController с базовым маршрутом для получения ответа от сервиса
    - Упростить FilesModule: удалить устаревшие зависимости и использовать новую структуру
    - Добавить базовый тест для FilesController, проверяющий метод getHello()
    - Обеспечить первоначальную реализацию структуры модуля Files для дальнейшего развития функциональности

--- apps/files/src/files.module.ts
+++ apps/files/src/files.module.ts
@@ -1,24 +1,12 @@
-import { DynamicModule, Module } from '@nestjs/common';
-import { FilesController } from './api/files.controller';
-import { FilesService } from './infrastructure/services/files.service';
-import { FilesConfig } from './files.config';
+import { Module } from '@nestjs/common';
 import { PrismaModule } from './database/prisma.module';
-import { StorageService } from './infrastructure/services/storage.service';
-import { FilesRepository } from './infrastructure/repository/files.repository';
-import { JwtStrategy } from './infrastructure/services/jwt.strategy';
-import { ApiSettings } from './setup/configuration/api-settings';
 import { CoreModule } from './core/core.module';
+import { FilesController } from './files.controller';
+import { FilesService } from './files.service';
 
 @Module({
   imports: [CoreModule, PrismaModule],
   controllers: [FilesController],
-  providers: [FilesService, FilesConfig, StorageService, FilesRepository, JwtStrategy],
+  providers: [FilesService],
 })
-export class FilesModule {
-  static async forRoot(apiSettings: ApiSettings): Promise<DynamicModule> {
-    return {
-      module: FilesModule,
-      imports: [],
-    };
-  }
-}
+export class FilesModule {}
