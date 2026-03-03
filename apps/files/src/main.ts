0a6ff888977c05c88a55d000cc983b19d6b07bd9
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-03 23:32:27 +0300

    refactor(files): упростить инициализацию файлового микросервиса
    - Заменить создание HTTP-приложения на создание контекста приложения через `NestFactory.createApplicationContext`
    - Использовать `ConfigService` для извлечения настроек `microserviceSettings` вместо прямого указания значений host и port
    - Переписать логику запуска микросервиса с использованием `NestFactory.createMicroservice`
    - Удалить устаревший код, связанный с настройкой proxy, HTTP-сервиса и файловой конфигурации
    - Улучшить читаемость и модульность кода, исключив неиспользуемые зависимости и упрощая структуру bootstrap функции

--- apps/files/src/main.ts
+++ apps/files/src/main.ts
@@ -1,33 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { FilesModule } from './files.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { appSetup } from './setup/app.setup';
-import { FilesConfig } from './files.config';
+import { ConfigService } from '@nestjs/config';
+import { Configuration } from './setup/configuration/configuration';
+import { MicroserviceSettings } from './setup/configuration/microservice.settings';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(FilesModule);
-  const server = app.getHttpAdapter().getInstance();
-  server.set('trust proxy', true);
+  const appContext = await NestFactory.createApplicationContext(FilesModule);
+  const configService = appContext.get<ConfigService<Configuration, true>>(ConfigService);
+  const microserviceSettings: MicroserviceSettings =
+    configService.get<MicroserviceSettings>('microserviceSettings');
+  await appContext.close();
 
-  app.connectMicroservice<MicroserviceOptions>({
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(FilesModule, {
     transport: Transport.TCP,
     options: {
-      host: '0.0.0.0',
-      port: 3002,
+      host: microserviceSettings.host,
+      port: microserviceSettings.port,
     },
   });
 
-  const filesConfig: FilesConfig = app.get<FilesConfig>(FilesConfig);
+  await app.listen();
 
-  appSetup(app, filesConfig);
-
-  await app.startAllMicroservices();
-  await app.listen(filesConfig.port, () => {
-    console.log('Files HTTP running on port:', filesConfig.port);
-    console.log('Files microservice running on port: 3002');
-    console.log('NODE_ENV:', filesConfig.env);
-  });
+  console.log(
+    `📦 Files microservice running on ${microserviceSettings.host}:${microserviceSettings.port}`,
+  );
 }
 
 void bootstrap();
