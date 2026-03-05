0968a3ac894679b6348f6c019b17c81500830dce
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-05 13:16:14 +0300

    feat(contracts): добавить валидатор для GenerateUploadUrlRequest
    - Создать класс GenerateUploadUrlInputDto для валидации данных запроса на генерацию URL загрузки
    - Добавить декораторы @IsNumber() для userId и size, @IsEnum(MimeType) для mimeType
    - Заменить строковый mimeType на тип MimeType для строгой типизации
    - Упростить интеграцию и обработку запросов благодаря централизованной валидации данных через DTO

--- libs/contracts/files/generate-upload-url.contract.ts
+++ libs/contracts/files/generate-upload-url.contract.ts
@@ -1,10 +1,24 @@
+import { IsEnum, IsNumber } from 'class-validator';
+import { MimeType } from './mime-type.enum';
+
 export interface GenerateUploadUrlRequest {
   userId: number;
-  mimeType: string;
+  mimeType: MimeType;
   size: number;
 }
 
 export interface GenerateUploadUrlResponse {
   uploadUrl: string;
   fileId: string;
+}
+
+export class GenerateUploadUrlInputDto implements GenerateUploadUrlRequest {
+  @IsNumber()
+  userId: number;
+
+  @IsEnum(MimeType)
+  mimeType: MimeType;
+
+  @IsNumber()
+  size: number;
 }
