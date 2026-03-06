05c135a74fadde7f4380a0c605c29a711c6c0e72
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-06 20:55:33 +0300

    refactor(files): удалить метод confirmUpload из PostsMediaService
    - Удалить метод confirmUpload из PostsMediaService, так как он не используется
    - Удалить зависимости DomainException и DomainExceptionCode, связанные с исключениями в этом методе
    - Упростить модуль media-files: устранить избыточный код и уменьшить количество импортов
    - Повысить читаемость и поддерживаемость кода через удаление неиспользуемой функциональности

--- apps/files/src/modules/media-files/post-media-files/application/services/posts-media.service.ts
+++ apps/files/src/modules/media-files/post-media-files/application/services/posts-media.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { DomainException } from '../../../../../../../../libs/common/exceptions/damain.exception';
-import { DomainExceptionCode } from '../../../../../../../../libs/common/exceptions/types/domain-exception-codes';
 import { StorageService } from '../../infrastructure/storage/storage.service';
 import { FilesRepository } from '../../infrastructure/repositories/files.repository';
 
@@ -10,31 +8,6 @@ export class PostsMediaService {
     private readonly storageService: StorageService,
     private readonly filesRepository: FilesRepository,
   ) {}
-
-  // для фронта после загрузки в хранилище
-  async confirmUpload(fileId: string, userId: number) {
-    const file = await this.filesRepository.findByIdAndUser(fileId, userId);
-
-    if (!file || file.status !== 'PENDING') {
-      // TODO разделить ошибки
-      throw new DomainException({
-        code: DomainExceptionCode.BadRequest,
-        message: 'Файл не найден или уже подтвержден', // TODO ошибки на англ
-      });
-      // throw new BadRequestException('Файл не найден или уже подтвержден');
-    }
-
-    const exist = await this.storageService.objectExists(file.key);
-
-    if (!exist) {
-      throw new DomainException({
-        code: DomainExceptionCode.BadRequest,
-        message: 'Файл не был загружен в хранилище', // TODO ошибки на англ
-      });
-      // throw new BadRequestException('файл не был загружен');
-    }
-    await this.filesRepository.confirmUpload(fileId);
-  }
 
   async validateFilesForPost(userId: number, fileIds: string[]) {
     if (!fileIds.length) return { valid: true, files: [] };
