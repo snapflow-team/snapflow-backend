fd079505d451b704ba35b6ef246884ef81231e81
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-09 19:04:08 +0300

    refactor(exceptions): добавить поддержку кастомной фабрики в ValidationExceptionFilter
    - Внедрить опциональную поддержку customFactory через конструктор ValidationExceptionFilter
    - Обновить метод catch(): использовать customFactory или стандартную errorResponseFactory для создания объекта ответа
    - Заменить использование ErrorResponseDto на интерфейс IErrorResponse для приведения к единому стандарту
    - Улучшить гибкость фильтра: добавить возможность замены стандартной фабрики пользовательской

--- libs/exceptions/http/filters/validation-http-exception.filter.ts
+++ libs/exceptions/http/filters/validation-http-exception.filter.ts
@@ -1,23 +1,24 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpStatus } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { ValidationException } from '../../core/validation-exception';
-import { ErrorResponseDto } from '../../../../apps/snapflow-core/src/common/exceptions/error-response-body.dto';
+import { IErrorResponse } from '../../core/error-response';
+import { errorResponseFactory, type IErrorResponseFactory, } from '../../core/error-response-factory';
 
 @Catch(ValidationException)
 export class ValidationExceptionFilter implements ExceptionFilter {
+  constructor(private readonly customFactory?: IErrorResponseFactory<string>) {}
+
   catch(exception: ValidationException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
 
-    const status = HttpStatus.BAD_REQUEST;
+    const status: number = HttpStatus.BAD_REQUEST;
 
-    const errorResponseDto: ErrorResponseDto = ErrorResponseDto.fromDomainException(
-      exception,
-      request.url,
-      request.method,
-    );
+    const errorResponseBody: IErrorResponse<string> = this.customFactory
+      ? this.customFactory(exception, request.url, request.method)
+      : errorResponseFactory(exception, request.url, request.method);
 
-    response.status(status).json(errorResponseDto);
+    response.status(status).json(errorResponseBody);
   }
 }
