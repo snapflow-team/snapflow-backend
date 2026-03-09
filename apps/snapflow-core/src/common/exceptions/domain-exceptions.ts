7b040eef5c08fc4f7825c22a31346d6b64ccb159
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-09 19:41:36 +0300

    refactor(exceptions): реорганизовать порядок классов в domain-exceptions.ts
    - Переместить класс UnauthorizedException ниже класса BadRequestException для логической группировки
    - Упорядочить объявления классов исключений для улучшения читаемости файла
    - Избежать случайных изменений за счет приведения структуры к единообразному виду

--- apps/snapflow-core/src/common/exceptions/domain-exceptions.ts
+++ apps/snapflow-core/src/common/exceptions/domain-exceptions.ts
@@ -1,22 +1,19 @@
 import { DomainException } from '../../../../../libs/exceptions/core';
-import {
-  SnapFlowDomainExceptionCode,
-  SnapFlowDomainExceptionCodeType,
-} from './domain-exception-codes';
-
-export class UnauthorizedException extends DomainException<SnapFlowDomainExceptionCodeType> {
-  constructor(message: string = 'Unauthorized') {
-    super({
-      code: SnapFlowDomainExceptionCode.Unauthorized,
-      message,
-    });
-  }
-}
+import { SnapFlowDomainExceptionCode, SnapFlowDomainExceptionCodeType, } from './domain-exception-codes';
 
 export class BadRequestException extends DomainException<SnapFlowDomainExceptionCodeType> {
   constructor(message: string = 'Bad Request') {
     super({
       code: SnapFlowDomainExceptionCode.BadRequest,
+      message,
+    });
+  }
+}
+
+export class UnauthorizedException extends DomainException<SnapFlowDomainExceptionCodeType> {
+  constructor(message: string = 'Unauthorized') {
+    super({
+      code: SnapFlowDomainExceptionCode.Unauthorized,
       message,
     });
   }
