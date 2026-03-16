e230ff17057471a64e262488669dc88d97af4d92
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-16 17:48:30 +0300

    refactor(config): удалить параметры аватаров из business-rules-settings
    - Удалить поля avatarImageSizeMb и avatarAllowedMimeTypes из BusinessRulesSettings
    - Удалить методы getAvatarImageSize и getAvatarAllowedMimeTypes, связанные с вышеуказанными параметрами
    - Упрощение конфигурации, так как валидация аватаров перенесена в другой компонент системы
    - Устранить ненужные зависимости от параметров окружения AVATAR_IMAGE_SIZE_MB и AVATAR_ALLOWED_MIME_TYPES

--- apps/snapflow-core/src/setup/configuration/business-rules-settings.ts
+++ apps/snapflow-core/src/setup/configuration/business-rules-settings.ts
@@ -11,12 +11,6 @@ export class BusinessRulesSettings {
   @IsNumber()
   sessionCleanupRetentionDays: number;
 
-  @IsNumber()
-  avatarImageSizeMb: number;
-
-  @IsString()
-  avatarAllowedMimeTypes: string;
-
   constructor(private readonly environmentVariables: EnvironmentVariable) {
     this.appEmail = environmentVariables.EMAIL_APP;
     this.appPassword = environmentVariables.EMAIL_APP_PASSWORD;
@@ -24,16 +18,5 @@ export class BusinessRulesSettings {
     this.sessionCleanupRetentionDays = Number.parseInt(
       environmentVariables.SESSION_CLEANUP_RETENTION_DAYS,
     );
-
-    this.avatarImageSizeMb = Number.parseInt(environmentVariables.AVATAR_IMAGE_SIZE_MB);
-    this.avatarAllowedMimeTypes = environmentVariables.AVATAR_ALLOWED_MIME_TYPES;
-  }
-
-  getAvatarImageSize(): number {
-    return this.avatarImageSizeMb * 1024 * 1024;
-  }
-
-  getAvatarAllowedMimeTypes(): string[] {
-    return this.avatarAllowedMimeTypes.split(',');
   }
 }
