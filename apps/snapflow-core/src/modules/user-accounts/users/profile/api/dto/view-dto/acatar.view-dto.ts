42077a7165b4826c29bf26938d209477209cf7ce
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-16 22:33:25 +0300

    feat(users): добавить документацию для AvatarViewDto
    - Добавить декоратор @ApiProperty для поля publicUrl в AvatarViewDto
    - Указать описание и пример значения publicUrl для генерации Swagger документации
    - Обеспечить ясность и удобство использования API благодаря полной документации состава данных в ответах

--- apps/snapflow-core/src/modules/user-accounts/users/profile/api/dto/view-dto/acatar.view-dto.ts
+++ apps/snapflow-core/src/modules/user-accounts/users/profile/api/dto/view-dto/acatar.view-dto.ts
@@ -1,3 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class AvatarViewDto {
+  @ApiProperty({
+    description: 'Публичный URL загруженного аватара пользователя',
+    example: 'https://cdn.example.com/avatars/123.jpg',
+  })
   publicUrl: string;
 }
