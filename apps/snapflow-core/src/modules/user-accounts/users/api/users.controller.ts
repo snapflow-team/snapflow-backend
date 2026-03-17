d2008f541b2f7f49db1be59ff5625649bddb9f5c
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-18 00:16:37 +0300

    feat(users): подключить ThrottlerGuard к endpoint общего числа пользователей
    - Добавить ThrottlerGuard к endpoint GET /users/total-count для ограничения запросов
    - Указать декоратор @UseGuards(ThrottlerGuard) для защиты от чрезмерных обращений
    - Улучшить защиту API от DDoS-атак и злоупотребления частыми запросами
    - Обеспечить соответствие ограничениям rate limiting (5 запросов за 10 секунд) через глобальные настройки ThrottlerModule

--- apps/snapflow-core/src/modules/user-accounts/users/api/users.controller.ts
+++ apps/snapflow-core/src/modules/user-accounts/users/api/users.controller.ts
@@ -1,10 +1,10 @@
 import { ApiTags } from '@nestjs/swagger';
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { QueryBus } from '@nestjs/cqrs';
 import { TotalCountRegisteredUsersViewDto } from './dto/view-dto/total-count-registered-users.view-dto';
-import {
-  GetTotalCountRegisteredUsersQuery
-} from '../application/queries/get-total-count-registered-users.query-handler';
+import { GetTotalCountRegisteredUsersQuery } from '../application/queries/get-total-count-registered-users.query-handler';
+import { ThrottlerGuard } from '@nestjs/throttler';
+import { ApiGetTotalUsersCount } from './swagger/count-all-users.swagger';
 
 @ApiTags('Users')
 @Controller('users')
@@ -12,6 +12,8 @@ export class UsersController {
   constructor(private readonly queryBus: QueryBus) {}
 
   @Get('total-count')
+  @ApiGetTotalUsersCount()
+  @UseGuards(ThrottlerGuard)
   async getTotalCount(): Promise<TotalCountRegisteredUsersViewDto> {
     return this.queryBus.execute(new GetTotalCountRegisteredUsersQuery());
   }
