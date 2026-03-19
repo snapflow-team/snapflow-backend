19ce7d156468920df14521ce8280061462131697
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-19 20:22:03 +0300

    fix...

--- apps/files/src/setup/configuration/database-settings.ts
+++ apps/files/src/setup/configuration/database-settings.ts
@@ -2,7 +2,6 @@ import { EnvironmentVariable } from './configuration';
 import { IsBoolean, IsString } from 'class-validator';
 import { Prisma } from '@generated/prisma-files';
 
-// todo: вынести в либу
 export class DatabaseSettings {
   @IsString()
   url: string;
