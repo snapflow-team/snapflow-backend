bad9bf5b9a11fca02edf523d75ffd5e9eb60a59c
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-19 15:25:04 +0300

    refactor(profile): удалить закомментированные поля из ProfileViewDto
    - Удалить ранее закомментированные поля followersCount, followingCount и postsCount из ProfileViewDto
    - Убрать todo-комментарии о переносе этих полей в public-dto
    - Оптимизировать структуру DTO для приведения к текущим архитектурным требованиям
    - Сократить объем лишнего кода, улучшив читабельность и поддерживаемость файла

--- apps/snapflow-core/src/modules/user-accounts/users/profile/api/dto/view-dto/profile.view-dto.ts
+++ apps/snapflow-core/src/modules/user-accounts/users/profile/api/dto/view-dto/profile.view-dto.ts
@@ -70,33 +70,6 @@ export class ProfileViewDto {
   })
   aboutMe: string | null;
 
-  // todo: перенести это поле в public-dto
-  // @ApiProperty({
-  //   type: Number,
-  //   description: 'Количество подписчиков',
-  //   example: 120,
-  //   default: 0,
-  // })
-  // followersCount: number = 0;
-
-  // todo: перенести это поле в public-dto
-  // @ApiProperty({
-  //   type: Number,
-  //   description: 'Количество подписок пользователя',
-  //   example: 80,
-  //   default: 0,
-  // })
-  // followingCount: number = 0;
-
-  // todo: перенести это поле в public-dto
-  // @ApiProperty({
-  //   type: Number,
-  //   description: 'Количество постов пользователя',
-  //   example: 34,
-  //   default: 0,
-  // })
-  // postsCount: number = 0;
-
   static mapToView(profile: UserProfile): ProfileViewDto {
     const dto = new this();
 
