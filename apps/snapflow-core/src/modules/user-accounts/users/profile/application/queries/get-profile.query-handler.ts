00e6886520c37d0f5fbd80928988ec733c9ed055
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-19 14:03:14 +0300

    refactor(profile): упростить обработку исключений в запросе профиля
    - Удалить зависимости от UsersRepository и NotFoundException в GetProfileQueryHandler
    - Обновить логику метода execute() для использования findProfileByUserIdOrNotFoundFail из ProfilesQueryRepository
    - Сократить количество импортов, улучшив читаемость и поддерживаемость кода
    - Упростить обработку ошибок за счет использования существующего метода для проверки профиля

--- apps/snapflow-core/src/modules/user-accounts/users/profile/application/queries/get-profile.query-handler.ts
+++ apps/snapflow-core/src/modules/user-accounts/users/profile/application/queries/get-profile.query-handler.ts
@@ -1,9 +1,6 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { ProfileViewDto } from '../../api/dto/view-dto/profile.view-dto';
 import { ProfilesQueryRepository } from '../../infrastructure/query/profiles.query-repository';
-import { UsersRepository } from '../../../infrastructure/users.repository';
-import { NotFoundException } from '../../../../../../common/exceptions/domain-exceptions';
-import { User } from '@generated/prisma-snapflow';
 
 export class GetProfileQuery {
   constructor(public readonly userId: number) {}
@@ -11,18 +8,9 @@ export class GetProfileQuery {
 
 @QueryHandler(GetProfileQuery)
 export class GetProfileQueryHandler implements IQueryHandler<GetProfileQuery, ProfileViewDto> {
-  constructor(
-    private readonly profilesQueryRepository: ProfilesQueryRepository,
-    private readonly usersRepository: UsersRepository,
-  ) {}
+  constructor(private readonly profilesQueryRepository: ProfilesQueryRepository) {}
 
   async execute({ userId }: GetProfileQuery): Promise<ProfileViewDto> {
-    const user: User | null = await this.usersRepository.findUserById(userId);
-
-    if (!user) {
-      throw new NotFoundException(`The user with ID (${userId}) does not exist`);
-    }
-
-    return this.profilesQueryRepository.findProfileByUserId(userId);
+    return this.profilesQueryRepository.findProfileByUserIdOrNotFoundFail(userId);
   }
 }
