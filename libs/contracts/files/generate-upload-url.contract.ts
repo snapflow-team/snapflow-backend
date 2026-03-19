9cbdd39318402ff73abea20f95fd0c42283f7a4d
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-19 18:10:52 +0300

    refactor(files): перенести GenerateUploadUrlViewDto в каталог view-dto
    - Переместить GenerateUploadUrlViewDto из libs/contracts/files в каталог view-dto модуля integrations/files
    - Обновить импорт GenerateUploadUrlViewDto в generate-upload-urls.swagger.ts для использования нового пути
    - Удалить устаревший код и комментарии из libs/contracts/files/generate-upload-url.contract.ts
    - Улучшить структуру проекта: теперь DTO находится ближе к месту его использования
    - Повысить читаемость и поддержку кода через ясное разделение ответственности между контрактами и ViewDto

--- libs/contracts/files/generate-upload-url.contract.ts
+++ libs/contracts/files/generate-upload-url.contract.ts
@@ -1,5 +1,4 @@
 import { MimeType } from './mime-type.enum';
-import { ApiProperty } from '@nestjs/swagger';
 
 export interface UploadFileRequestForGenerateUploadUrls {
   mimeType: MimeType;
@@ -13,22 +12,5 @@ export interface GenerateUploadUrlsRequest {
 
 export interface GenerateUploadUrlResponse {
   uploadUrl: string;
-  fileId: string;
-}
-
-// TODO Вынести в files
-
-export class GenerateUploadUrlViewDto implements GenerateUploadUrlResponse {
-  @ApiProperty({
-    format: 'uri',
-    description: 'Подписанная ссылка для загрузки файла',
-    example: 'https://storage/...signed-url',
-  })
-  uploadUrl: string;
-
-  @ApiProperty({
-    format: 'uuid',
-    description: 'Идентификатор файла',
-  })
   fileId: string;
 }
