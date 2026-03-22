aab64af2634e9a9a650976c9f9b1bec83a61dd65
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-22 20:03:03 +0300

    refactor(integrations): заменить AuthTokenService на CryptoService в NextjsRevalidationService
    - Заменить AuthTokenService на CryptoService для генерации JWT токенов
    - Перенести метод generateWebhookToken из AuthTokenService в CryptoService и переименовать его в generateJwtToken
    - Подключить CryptoService в NextjsIntegrationModule для обеспечения доступности нового функционала
    - Удалить generateWebhookToken из AuthTokenService, так как его функционал перенесен
    - Улучшить структуру и консистентность использования сервисов для работы с токенами

--- apps/snapflow-core/src/modules/integrations/nextjs/nextjs-integration.module.ts
+++ apps/snapflow-core/src/modules/integrations/nextjs/nextjs-integration.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { NextjsRevalidationService } from './nextjs-revalidation.service';
 import { RevalidateOnPostCreatedEventHandler } from './event-handlers/revalidate-on-post-created.event-handler';
+import { CryptoService } from '../../../../../../libs/common/services/crypto.service';
 
 @Module({
   imports: [HttpModule],
-  providers: [NextjsRevalidationService, RevalidateOnPostCreatedEventHandler],
+  providers: [NextjsRevalidationService, RevalidateOnPostCreatedEventHandler, CryptoService],
 })
 export class NextjsIntegrationModule {}
--- apps/snapflow-core/src/modules/integrations/nextjs/nextjs-revalidation.service.ts
+++ apps/snapflow-core/src/modules/integrations/nextjs/nextjs-revalidation.service.ts
@@ -6,9 +6,9 @@ import { lastValueFrom } from 'rxjs';
 import { ApiSettings } from '../../../setup/configuration/api-settings';
 import { REDIS_CLIENT_INJECT_TOKEN } from '../../../core/providers/provide-tokens/redis-client.inject-token';
 import { Configuration } from '../../../setup/configuration/configuration';
-import { AuthTokenService } from '../../../../../../libs/common/services/auth-token.service';
 import * as jwt from 'jsonwebtoken';
 import { NextjsEndpoints } from './constants/nextjs-endpoints';
+import { CryptoService } from '../../../../../../libs/common/services/crypto.service';
 
 @Injectable()
 export class NextjsRevalidationService {
@@ -18,7 +18,7 @@ export class NextjsRevalidationService {
   constructor(
     @Inject(REDIS_CLIENT_INJECT_TOKEN) private readonly redis: Redis,
     private readonly httpService: HttpService,
-    private readonly tokensService: AuthTokenService,
+    private readonly cryptoService: CryptoService,
     private readonly configService: ConfigService<Configuration, true>,
   ) {
     this.apiSettings = this.configService.get<ApiSettings>('apiSettings');
@@ -41,7 +41,7 @@ export class NextjsRevalidationService {
       const expiresIn = this.apiSettings
         .nextjsRevalidationTokenExpiresIn as jwt.SignOptions['expiresIn'];
 
-      const token: string = this.tokensService.generateWebhookToken(
+      const token: string = this.cryptoService.generateJwtToken(
         { action: 'revalidate_home' },
         secret,
         expiresIn,
