48222e284f5fd77cc6c9d5ed1d7336341031e6fc
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-23 15:58:46 +0300

    add todo

--- apps/snapflow-core/test/01-auth/registration-confirmation.e2e-spec.ts
+++ apps/snapflow-core/test/01-auth/registration-confirmation.e2e-spec.ts
@@ -3,12 +3,16 @@ import { AuthTestManager } from '../managers/auth.test-manager';
 import { Server } from 'http';
 import { EmailService } from '../../src/modules/emails/services/email.service';
 import { EmailTemplate } from '../../src/modules/emails/templates/types';
-import { RegistrationEmailResendingInputDto } from '../../src/modules/user-accounts/auth/api/input-dto/registration-email-resending.input-dto';
+import {
+  RegistrationEmailResendingInputDto
+} from '../../src/modules/user-accounts/auth/api/input-dto/registration-email-resending.input-dto';
 import { TestDtoFactory } from '../helpers/test.dto-factory';
 import request from 'supertest';
 import { GLOBAL_PREFIX } from '../../../../libs/common/constants/global-prefix.constant';
 import { HttpStatus } from '@nestjs/common';
-import { RegistrationUserInputDto } from '../../src/modules/user-accounts/auth/api/input-dto/registration-user.input-dto';
+import {
+  RegistrationUserInputDto
+} from '../../src/modules/user-accounts/auth/api/input-dto/registration-user.input-dto';
 import { ConfirmationStatus } from '@generated/prisma';
 import { UserWithEmailConfirmation } from '../../src/modules/user-accounts/users/types/user-with-confirmation.type';
 
@@ -35,7 +39,7 @@ describe('AuthController registratinEmailResending POST: /auth/registration-emai
     await appTestManager.close();
   });
 
-  // todo: если запускаешь все e2e тесты, то этот тест падает!
+  // todo(magomed): если запускаешь все e2e тесты, то этот тест падает!
   it('должен отправить письмо подтверждения, если emil корректный и пользователь не подтвержден', async () => {
     const registrationDtos: RegistrationUserInputDto[] =
       TestDtoFactory.generateRegistrationUserInputDto(1);
