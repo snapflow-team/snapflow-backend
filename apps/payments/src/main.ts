614c973c69fa78a0d0ef604174ed030316d52ec4
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-26 12:06:26 +0300

    feat(payments): добавить логирование и настройку порта через ConfigService
    - Добавить Logger для вывода информации о запуске приложения
    - Использовать ConfigService для получения настроек apiSettings и environmentSettings
    - Изменить процесс запуска: использовать порт из apiSettings вместо process.env.port
    - Указать текущее окружение в логах с использованием environmentSettings
    - Улучшить информативность при запуске приложения через цветовое выделение в логах

--- apps/payments/src/main.ts
+++ apps/payments/src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { PaymentsModule } from './payments.module';
+import { ConfigService } from '@nestjs/config';
+import { Configuration } from './setup/configuration/configuration';
+import { ApiSettings } from './setup/configuration/api-settings';
+import { EnvironmentSettings } from './setup/configuration/environment-settings';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+
   const app = await NestFactory.create(PaymentsModule);
-  await app.listen(process.env.port ?? 3003);
+
+  const configService: ConfigService<Configuration, true> = app.get(
+    ConfigService<Configuration, true>,
+  );
+  const apiSettings: ApiSettings = configService.get<ApiSettings>('apiSettings');
+  const environmentSettings: EnvironmentSettings =
+    configService.get<EnvironmentSettings>('environmentSettings');
+
+  const port: number = apiSettings.port;
+  const env: string = environmentSettings.currentEnv;
+
+  await app.listen(port, () => {
+    logger.log(`\x1b[35m=========================================\x1b[0m`);
+    logger.log(`\x1b[36m✅ Application is running in ${env} mode\x1b[0m`);
+    logger.log(`\x1b[36m📡 Server listening on port ${port}\x1b[0m`);
+    logger.log(`\x1b[36m🌍 Environment: ${env}\x1b[0m`);
+    logger.log(`\x1b[35m=========================================\x1b[0m`);
+  });
 }
-bootstrap();
+
+void bootstrap();
