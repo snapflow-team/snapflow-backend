9d6f101ae384a8555d947fc9589f7f68058157c6
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-27 19:36:20 +0300

    feat(core): внедрить динамическое определение baseUrl для email шаблонов
    - Добавить использование ConfigService для получения конфигурации окружения в EmailTemplates
    - Определить baseUrl на основе текущего окружения: 'https://snapflow.cc' для production и 'http://localhost:3000' для остальных
    - Изменить ссылки в registrationEmail и passwordRecoveryEmail для подстановки baseUrl
    - Обеспечить унифицированность ссылок в email шаблонах, повышая гибкость и удобство настройки в разных средах разработки

--- apps/snapflow-core/src/modules/emails/templates/email.templates.ts
+++ apps/snapflow-core/src/modules/emails/templates/email.templates.ts
@@ -1,25 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { EmailTemplate } from './types';
+import { ConfigService } from '@nestjs/config';
+import { Configuration } from '../../../setup/configuration/configuration';
+import { EnvironmentSettings } from '../../../setup/configuration/environment-settings';
 
 @Injectable()
 export class EmailTemplates {
+  private readonly baseUrl: string;
+
+  constructor(private readonly configService: ConfigService<Configuration, true>) {
+    const environmentSettings = this.configService.get<EnvironmentSettings>('environmentSettings');
+
+    // Определяем базовый URL один раз при инициализации сервиса
+    this.baseUrl = environmentSettings.isProduction
+      ? 'https://snapflow.cc'
+      : 'http://localhost:3000';
+  }
+
   registrationEmail(code: string): EmailTemplate {
     return {
       subject: 'Confirmation of registration',
-      html: ` <h1>Thanks for your registration</h1>
-               <p>To finish registration please follow the link below:<br>
-                  <a href='https://snapflow.cc/sign-up/confirm-email?code=${code}'>complete registration</a>
-              </p>`,
+      html: `
+        <h1>Thanks for your registration</h1>
+        <p>To finish registration please follow the link below:<br>
+          <a href='${this.baseUrl}/sign-up/confirm-email?code=${code}'>complete registration</a>
+        </p>
+      `,
     };
   }
 
   passwordRecoveryEmail(code: string): EmailTemplate {
     return {
       subject: 'Password recovery',
-      html: ` <h1>Password recovery</h1>
-               <p>To finish password recovery please follow the link below:
-            <a href='https://snapflow.cc/password-recovery/set-new-password?recoveryCode=${code}'>recovery password</a>
-        </p>`,
+      html: `
+        <h1>Password recovery</h1>
+        <p>To finish password recovery please follow the link below:<br>
+          <a href='${this.baseUrl}/password-recovery/set-new-password?recoveryCode=${code}'>recovery password</a>
+        </p>
+      `,
     };
   }
 }
