f52b607dfa99aad447ff428a5c738ab72ee917be
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-29 22:41:04 +0300

    feat(payments): добавить код исключения Success в PaymentsDomainExceptionCode
    - Расширить объект PaymentsDomainExceptionCode, добавив новое поле `Success: 'Success'`
    - Обеспечить представление успешного результата через код исключения
    - Улучшить консистентность работы с кодами исключений и уведомлениями в платежном модуле

--- apps/payments/src/common/exceptions/domain-exception-codes.ts
+++ apps/payments/src/common/exceptions/domain-exception-codes.ts
@@ -2,6 +2,7 @@ import { CommonDomainExceptionCode } from '../../../../../libs/exceptions/core';
 
 export const PaymentsDomainExceptionCode = {
   ...CommonDomainExceptionCode,
+  Success: 'Success',
 } as const;
 
 export type PaymentsDomainExceptionCodeType =
