5efd9becf229386f1c0d544a4cca841b46fc7384
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-30 12:01:10 +0300

    refactor(payments): обновить исключения для использования NotificationResultCode
    - Заменить PaymentsDomainExceptionCode на NotificationResultCode в BadRequestException, UnauthorizedException и InternalServerErrorException
    - Добавить опциональное поле extensions в конструктора исключений для расширения данных
    - Удалить устаревшие импорты и закомментированный код, связанный с PaymentsDomainExceptionCode
    - Улучшить стандартизацию обработки исключений и соответствие NotificationResultCode типов

--- apps/payments/src/common/exceptions/domain-exceptions.ts
+++ apps/payments/src/common/exceptions/domain-exceptions.ts
@@ -1,20 +1,53 @@
-import { DomainException } from '../../../../../libs/exceptions/core';
-import { PaymentsDomainExceptionCode, PaymentsDomainExceptionCodeType, } from './domain-exception-codes';
+import { DomainException, IExtension } from '../../../../../libs/exceptions/core';
+import { NotificationResultCode, NotificationResultCodeType, } from '../notification/notification-result-code';
 
-export class BadRequestException extends DomainException<PaymentsDomainExceptionCodeType> {
-  constructor(message: string = 'Bad Request') {
+export class BadRequestException extends DomainException<NotificationResultCodeType> {
+  constructor(message: string = 'Bad Request', extensions?: IExtension[]) {
     super({
-      code: PaymentsDomainExceptionCode.BadRequest,
+      code: NotificationResultCode.BadRequest,
       message,
+      extensions,
     });
   }
 }
 
-export class UnauthorizedException extends DomainException<PaymentsDomainExceptionCodeType> {
-  constructor(message: string = 'Unauthorized') {
+export class UnauthorizedException extends DomainException<NotificationResultCodeType> {
+  constructor(message: string = 'Unauthorized', extensions?: IExtension[]) {
     super({
-      code: PaymentsDomainExceptionCode.Unauthorized,
+      code: NotificationResultCode.Unauthorized,
       message,
+      extensions,
     });
   }
 }
+
+export class InternalServerErrorException extends DomainException<NotificationResultCodeType> {
+  constructor(message: string = 'Some error occurred', extensions?: IExtension[]) {
+    super({
+      code: NotificationResultCode.InternalServerError,
+      message,
+      extensions,
+    });
+  }
+}
+
+// import { DomainException } from '../../../../../libs/exceptions/core';
+// import { PaymentsDomainExceptionCode, PaymentsDomainExceptionCodeType, } from './domain-exception-codes';
+//
+// export class BadRequestException extends DomainException<PaymentsDomainExceptionCodeType> {
+//   constructor(message: string = 'Bad Request') {
+//     super({
+//       code: PaymentsDomainExceptionCode.BadRequest,
+//       message,
+//     });
+//   }
+// }
+//
+// export class UnauthorizedException extends DomainException<PaymentsDomainExceptionCodeType> {
+//   constructor(message: string = 'Unauthorized') {
+//     super({
+//       code: PaymentsDomainExceptionCode.Unauthorized,
+//       message,
+//     });
+//   }
+// }
