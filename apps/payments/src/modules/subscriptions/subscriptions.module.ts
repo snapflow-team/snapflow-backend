409d4ebc1ffbfb8e7e79d5a3acf7ac1246948bf6
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-30 12:06:43 +0300

    feat(subscriptions): добавить зависимости в SubscriptionsModule
    - Добавить StripeService в providers для обеспечения взаимодействия со Stripe API
    - Зарегистрировать CreateCheckoutSessionUseCase в providers для обработки команд на создание checkout session
    - Включить SubscriptionsRepository в providers для работы с данными подписок
    - Обеспечить корректную модульную структуру для реализации функционала подписок

--- apps/payments/src/modules/subscriptions/subscriptions.module.ts
+++ apps/payments/src/modules/subscriptions/subscriptions.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
 import { SubscriptionsController } from './api/subscriptions.controller';
 import { GetPlansQueryHandler } from './application/queries/get-plans.query-handler';
+import { CreateCheckoutSessionUseCase } from './application/usecases/create-checkout-session.usecase';
+import { StripeService } from './application/services/stripe.service';
+import { SubscriptionsRepository } from './infrastructure/subscriptions.repository';
 
 const controllers = [SubscriptionsController];
+const useCases = [CreateCheckoutSessionUseCase];
 const queries = [GetPlansQueryHandler];
+const services = [StripeService];
+const repositories = [SubscriptionsRepository];
 
 @Module({
   imports: [],
   controllers: [...controllers],
-  providers: [...queries],
+  providers: [...useCases, ...queries, ...services, ...repositories],
   exports: [],
 })
 export class SubscriptionsModule {}
