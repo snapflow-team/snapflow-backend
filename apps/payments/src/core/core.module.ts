c4550202e4efae692741fc8b9637412034c03d1f
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-30 12:00:00 +0300

    feat(core): добавить HttpModule и NotificationResultCodeMapper в CoreModule
    - Импортировать HttpModule в CoreModule для работы с HTTP-запросами
    - Заменить PaymentsDomainExceptionCodeMapper на NotificationResultCodeMapper в providers
    - Экспортировать HttpModule и NotificationResultCodeMapper для использования в других модулях
    - Удалить устаревший PaymentsDomainExceptionCodeMapper и его экспорт
    - Обеспечить готовность CoreModule для работы с уведомлениями и HTTP-запросами

--- apps/payments/src/core/core.module.ts
+++ apps/payments/src/core/core.module.ts
@@ -4,12 +4,14 @@ import { ConfigModule } from '@nestjs/config';
 import configuration, { loadEnv, validate } from '../setup/configuration/configuration';
 import { RedisProvider } from './providers/redis.provider';
 import { REDIS_CLIENT_INJECT_TOKEN } from './providers/provide-tokens/redis-client.inject-token';
-import { PaymentsDomainExceptionCodeMapper } from '../common/exceptions/payments-domain-exception-mapper';
+import { HttpModule } from '@nestjs/axios';
+import { NotificationResultCodeMapper } from '../common/notification/notification-result-code.mapper';
 
 /* Глобальный модуль для провайдеров и модулей необходимых во всех частях приложения (например: LoggerService, CqrsModule, etc...) */
 @Global()
 @Module({
   imports: [
+    HttpModule,
     CqrsModule,
     ConfigModule.forRoot({
       isGlobal: true,
@@ -18,7 +20,7 @@ import { PaymentsDomainExceptionCodeMapper } from '../common/exceptions/payments
       envFilePath: loadEnv(),
     }),
   ],
-  providers: [PaymentsDomainExceptionCodeMapper, RedisProvider],
-  exports: [CqrsModule, PaymentsDomainExceptionCodeMapper, REDIS_CLIENT_INJECT_TOKEN],
+  providers: [NotificationResultCodeMapper, RedisProvider],
+  exports: [HttpModule, CqrsModule, NotificationResultCodeMapper, REDIS_CLIENT_INJECT_TOKEN],
 })
 export class CoreModule {}
