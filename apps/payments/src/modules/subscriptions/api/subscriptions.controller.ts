68fa5869e5aade89c7565787023425abf0157831
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-30 12:06:29 +0300

    feat(subscriptions): добавить создание Stripe checkout session
    - Добавить endpoint POST /subscriptions/stripe/checkout-session в SubscriptionsController
    - Реализовать защиту эндпоинта с помощью RemoteAuthGuard
    - Добавить декоратор @ExtractUserFromRequest для извлечения текущего пользователя
    - Реализовать метод createCheckoutSession с использованием CreateCheckoutSessionCommand
    - Обрабатывать уведомления с ошибками через NotificationExceptionMapper
    - Возвращать объект с url на страницу оплаты после успешного выполнения команды

--- apps/payments/src/modules/subscriptions/api/subscriptions.controller.ts
+++ apps/payments/src/modules/subscriptions/api/subscriptions.controller.ts
@@ -1,14 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { QueryBus } from '@nestjs/cqrs';
 import { GetPlansQuery } from '../application/queries/get-plans.query-handler';
 import { PlanViewDto } from './view-dto/plan.view-dto';
+import { RemoteAuthGuard } from '../../auth/guards/remote-auth.guard';
+import { CreateCheckoutSessionInputDto } from './input-dto/create-checkout-session.input-dto';
+import { UserContextDto } from '../../auth/guards/dto/user-context.dto';
+import { ExtractUserFromRequest } from '../../auth/guards/decorators/extract-user-from-request.decorator';
+import { Notification } from '../../../common/notification/notification';
+import { CreateCheckoutSessionCommand } from '../application/usecases/create-checkout-session.usecase';
+import { NotificationExceptionMapper } from '../../../common/notification/notification-exception.mapper';
 
 @Controller('subscriptions')
 export class SubscriptionsController {
-  constructor(private readonly queryBus: QueryBus) {}
+  constructor(
+    private readonly queryBus: QueryBus,
+    private readonly commandBus: QueryBus,
+  ) {}
 
   @Get('plans')
   async getPlans(): Promise<PlanViewDto[]> {
     return this.queryBus.execute(new GetPlansQuery());
+  }
+
+  @UseGuards(RemoteAuthGuard)
+  @Post('stripe/checkout-session')
+  async createCheckoutSession(
+    @Body() { planId }: CreateCheckoutSessionInputDto,
+    @ExtractUserFromRequest() { id: userId }: UserContextDto,
+  ) {
+    const notification: Notification<string> = await this.commandBus.execute<
+      CreateCheckoutSessionCommand,
+      Notification<string>
+    >(new CreateCheckoutSessionCommand({ userId, planId }));
+
+    if (notification.hasErrors) {
+      NotificationExceptionMapper.throw(notification);
+    }
+
+    return { url: notification.value };
   }
 }
