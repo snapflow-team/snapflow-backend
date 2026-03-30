aa965ad2297d023de2a286b8e118a16b7143f439
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-03-30 12:00:55 +0300

    refactor(payments): удалить PaymentsDomainExceptionCode
    - Удалить объект PaymentsDomainExceptionCode как неиспользуемый
    - Удалить тип PaymentsDomainExceptionCodeType, связанный с этим объектом
    - Исключить избыточный код для упрощения поддержки и улучшения читаемости
    - Подготовить модуль к полному переходу на NotificationResultCode

--- apps/payments/src/common/exceptions/domain-exception-codes.ts
+++
@@ -1,8 +0,0 @@
-import { CommonDomainExceptionCode } from '../../../../../libs/exceptions/core';
-
-export const PaymentsDomainExceptionCode = {
-  ...CommonDomainExceptionCode,
-} as const;
-
-export type PaymentsDomainExceptionCodeType =
-  (typeof PaymentsDomainExceptionCode)[keyof typeof PaymentsDomainExceptionCode];
