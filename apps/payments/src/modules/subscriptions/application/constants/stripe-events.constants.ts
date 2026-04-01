82b97b53d30b5e121948a95c757982ebe9003796
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-04-01 19:52:26 +0300

    feat(payments): добавить константы событий Stripe
    - Создать StripeEvents в stripe-events.constants.ts для хранения ключевых событий
    - Добавить CheckoutSessionCompleted, CustomerSubscriptionUpdated, PaymentIntentSucceeded
    - Использовать значения событий Stripe для стандартизации обработки вебхуков
    - Обеспечить единый источник правды для имен событий, исключая дублирование

---
+++ apps/payments/src/modules/subscriptions/application/constants/stripe-events.constants.ts
@@ -0,0 +1,5 @@
+export const StripeEvents = {
+  CheckoutSessionCompleted: 'checkout.session.completed',
+  CustomerSubscriptionUpdated: 'customer.subscription.updated',
+  PaymentIntentSucceeded: 'payment_intent.succeeded',
+} as const;
