65003cd1f9c9ae3e3a1c27d660084dcafe793aca
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-04-01 19:52:07 +0300

    feat(payments): уточнить возвращаемое значение findByExternalId
    - Добавить возвращаемый тип Promise<Payment | null> для метода findByExternalId
    - Уточнить тип возвращаемого объекта для улучшения читаемости и безопасной работы с результатами
    - Использовать импорт класса Payment из @generated/prisma-payments для соблюдения типизации
    - Упростить работу с платежами, избегая неявного указания типа результатов поиска

--- apps/payments/src/modules/subscriptions/infrastructure/payments.repository.ts
+++ apps/payments/src/modules/subscriptions/infrastructure/payments.repository.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../database/prisma.service';
-import { PaymentStatus, Prisma } from '@generated/prisma-payments';
+import { Payment, PaymentStatus, Prisma } from '@generated/prisma-payments';
 
 @Injectable()
 export class PaymentsRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  async findByExternalId(externalId: string, tx: Prisma.TransactionClient = this.prisma) {
+  async findByExternalId(
+    externalId: string,
+    tx: Prisma.TransactionClient = this.prisma,
+  ): Promise<Payment | null> {
     return tx.payment.findFirst({
       where: { externalId },
     });
