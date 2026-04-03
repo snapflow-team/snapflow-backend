22ff61b265c6df46f65ab5b7de8094670713f1ce
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-04-03 22:56:02 +0300

    feat(payments): обновить методы работы с событиями outbox
    - Переименовать метод getPendingBatchAndLock в lockEventsForProcessing для большей ясности
    - Изменить SQL-запрос в lockEventsForProcessing: добавлена установка статуса PROCESSING и обновление времени updated_at
    - Добавить метод recoverStaleEvents для восстановления событий со статусом PROCESSING, если они устарели
    - Использовать константы OutboxProcessing для значений LOCK_BATCH_SIZE и STALE_THRESHOLD_MINUTES
    - Повысить надежность управления статусами событий и упростить масштабирование логики обработки

--- apps/payments/src/modules/outbox/repositories/outbox.repository.ts
+++ apps/payments/src/modules/outbox/repositories/outbox.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { OutboxEvent, OutboxEventStatus, OutboxEventType, Prisma, } from '@generated/prisma-payments';
 import { PrismaService } from '../../database/prisma.service';
+import { OutboxProcessing } from '../constants/outbox.constants';
 
 @Injectable()
 export class OutboxRepository {
@@ -16,14 +17,34 @@ export class OutboxRepository {
     });
   }
 
-  async getPendingBatchAndLock(limit: number = 50): Promise<OutboxEvent[]> {
+  async lockEventsForProcessing(
+    limit: number = OutboxProcessing.LOCK_BATCH_SIZE,
+  ): Promise<OutboxEvent[]> {
     return this.prisma.$queryRaw<OutboxEvent[]>`
-      SELECT * FROM "outbox_events"
-      WHERE status = ${OutboxEventStatus.PENDING}
-      ORDER BY "created_at" ASC
-      LIMIT ${limit}
-      FOR UPDATE SKIP LOCKED;
+      UPDATE "outbox_events"
+      SET
+        "status" = ${OutboxEventStatus.PROCESSING},
+        "updated_at" = NOW()
+      WHERE "id" IN (
+        SELECT "id" FROM "outbox_events"
+        WHERE "status" = ${OutboxEventStatus.PENDING}
+        ORDER BY "created_at" ASC
+        LIMIT ${limit}
+        FOR UPDATE SKIP LOCKED
+        )
+        RETURNING *;
     `;
+  }
+
+  async recoverStaleEvents(
+    staleThresholdMinutes: number = OutboxProcessing.STALE_THRESHOLD_MINUTES,
+  ): Promise<number> {
+    return this.prisma.$executeRaw`
+    UPDATE "outbox_events"
+    SET "status" = ${OutboxEventStatus.PENDING}
+    WHERE "status" = ${OutboxEventStatus.PROCESSING}
+      AND "updated_at" < NOW() - make_interval(mins => ${staleThresholdMinutes});
+  `;
   }
 
   async markAsProcessed(id: string): Promise<void> {
