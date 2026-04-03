6bc11ba6388b8be010ecde67a8acefd31c13d311
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-04-03 23:25:45 +0300

    refactor(outbox): удалить неиспользуемый импорт PrismaModule
    - Удалить импорт PrismaModule из OutboxModule, так как он больше не используется
    - Упростить структуру модуля и избавиться от лишней зависимости
    - Обеспечить чистоту и поддержку кода через удаление ненужных элементов

--- apps/payments/src/modules/outbox/outbox.module.ts
+++ apps/payments/src/modules/outbox/outbox.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from '../database/prisma.module';
 import { OutboxRepository } from './repositories/outbox.repository';
 import { OutboxProcessorService } from './services/outbox-processor.service';
 import { RabbitMQPublisherService } from './services/rabbitmq-publisher.service';
 
 @Module({
-  imports: [PrismaModule],
   providers: [OutboxRepository, RabbitMQPublisherService, OutboxProcessorService],
   // refactor: экспортировать дубликат репозитория только с необходимыми методами!
   exports: [OutboxRepository],
