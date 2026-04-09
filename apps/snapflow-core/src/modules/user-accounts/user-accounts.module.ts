38d0dee31654ec4ca4d57e5da35263a9811bd589
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-04-09 23:25:42 +0300

    feat(users): экспортировать UsersRepository из UserAccountsModule
    - Добавить UsersRepository в массив exports модуля UserAccountsModule для предоставления доступа внешним модулям
    - Устранить ограничение на использование UsersRepository за пределами модуля
    - Обеспечить удобство использования UsersRepository в других модулях, например, для запросов и работы с данными пользователей
    - Упростить интеграцию и тестирование функциональности через расширение экспорта модуля

--- apps/snapflow-core/src/modules/user-accounts/user-accounts.module.ts
+++ apps/snapflow-core/src/modules/user-accounts/user-accounts.module.ts
@@ -57,9 +57,7 @@ import { FilesMediaController } from '../integrations/files/api/files-media.cont
 import { MulterModule } from '@nestjs/platform-express';
 import { UploadAvatarUseCase } from './users/profile/application/usecases/upload-avatar.usecase';
 import { DeleteAvatarUseCase } from './users/profile/application/usecases/delete-avatar.usecase';
-import {
-  GetTotalCountRegisteredUsersQueryHandler
-} from './users/application/queries/get-total-count-registered-users.query-handler';
+import { GetTotalCountRegisteredUsersQueryHandler } from './users/application/queries/get-total-count-registered-users.query-handler';
 import { UsersController } from './users/api/users.controller';
 import { GetPublicProfileQueryHandler } from './users/profile/application/queries/get-public-profile.query-handler';
 import { ConfigService } from '@nestjs/config';
@@ -166,6 +164,6 @@ const strategies = [LocalStrategy, JwtStrategy, JwtRefreshStrategy, GoogleStrate
     ...repositories,
     ...strategies,
   ],
-  exports: [],
+  exports: [UsersRepository],
 })
 export class UserAccountsModule {}
