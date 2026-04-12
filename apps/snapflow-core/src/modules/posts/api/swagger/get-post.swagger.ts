29c9e509937014dfd288c76d0866bd22e62aa8dc
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-04-12 23:30:36 +0300

    refactor(posts): упрощение метода GetPostByIdSwagger
    - Удалить декоратор ApiBearerAuth из GetPostByIdSwagger, отразив публичную доступность метода
    - Обновить ApiOperation: заменить описание на "Получить пост по id" для унификации
    - Исключить декораторы ApiUnauthorizedResponse, связанные с аутентификацией, так как она больше не требуется
    - Сократить и оптимизировать импорты из @nestjs/swagger

--- apps/snapflow-core/src/modules/posts/api/swagger/get-post.swagger.ts
+++ apps/snapflow-core/src/modules/posts/api/swagger/get-post.swagger.ts
@@ -1,28 +1,10 @@
 ﻿import { applyDecorators } from '@nestjs/common';
-import {
-  ApiBearerAuth,
-  ApiNotFoundResponse,
-  ApiOkResponse,
-  ApiOperation,
-  ApiParam,
-  ApiUnauthorizedResponse,
-} from '@nestjs/swagger';
+import { ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiParam } from '@nestjs/swagger';
 import { PostViewDto } from '../view-dto/post.view-dto';
 
 export function GetPostByIdSwagger() {
   return applyDecorators(
-    ApiBearerAuth('access-token'),
-    ApiOperation({ summary: 'Получить свой пост (черновик или опубликованный)' }),
-    ApiParam({ name: 'id', type: String, description: 'Идентификатор поста' }),
-    ApiOkResponse({ description: 'Пост найден', type: PostViewDto }),
-    ApiNotFoundResponse({ description: 'Пост не найден' }),
-    ApiUnauthorizedResponse({ description: 'Пользователь не авторизован' }),
-  );
-}
-
-export function GetPublicPostSwagger() {
-  return applyDecorators(
-    ApiOperation({ summary: 'Получить публичный пост по id' }),
+    ApiOperation({ summary: 'Получить пост по id' }),
     ApiParam({ name: 'id', type: String, description: 'Идентификатор поста' }),
     ApiOkResponse({ description: 'Пост найден', type: PostViewDto }),
     ApiNotFoundResponse({ description: 'Пост не найден' }),
