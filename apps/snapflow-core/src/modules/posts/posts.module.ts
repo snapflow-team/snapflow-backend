31ecdbe696413343ff3746f46e9ee54771815c34
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-04-13 13:05:07 +0300

    refactor(posts): заменить GetMyDraftsHandler на GetMyDraftHandler и добавить SaveDraftUseCase
    - Переименовать GetMyDraftsHandler в GetMyDraftHandler для уточнения логики получения одного черновика
    - Удалить устаревший GetMyDraftsHandler из списка queries
    - Добавить SaveDraftUseCase в список useCases для обработки сохранения черновиков
    - Обновить PostsModule: упростить работу с черновиками за счет применения новых handler и use case

--- apps/snapflow-core/src/modules/posts/posts.module.ts
+++ apps/snapflow-core/src/modules/posts/posts.module.ts
@@ -1,7 +1,7 @@
 import { DeletePostUseCase } from './application/usecases/delete-post.use.case';
 import { EditPostUseCase } from './application/usecases/edit-post.use.case';
 import { CreatePostUseCase } from './application/usecases/create-post-use.case';
-import { GetMyDraftsHandler } from './application/queries/get-my-drafts.query.handler';
+import { GetMyDraftHandler } from './application/queries/get-my-draft.query.handler';
 import { GetPostsQueryHandler } from './application/queries/get-posts.query-handler';
 import { GetPostQueryHandler } from './application/queries/get-post.query-handler';
 import { PostsRepository } from './infrastructure/posts-repository';
@@ -11,12 +11,13 @@ import { PostsController } from './api/posts.controller';
 import { UserAccountsModule } from '../user-accounts/user-accounts.module';
 import { FilesClientModule } from '../integrations/files/files-client.module';
 import { GetProfilePostsQueryHandler } from './application/queries/get-profile-posts.query-handler';
+import { SaveDraftUseCase } from './application/usecases/save-draft.usecase';
 
-const useCases = [CreatePostUseCase, EditPostUseCase, DeletePostUseCase];
+const useCases = [CreatePostUseCase, EditPostUseCase, DeletePostUseCase, SaveDraftUseCase];
 const queries = [
   GetPostQueryHandler,
   GetPostsQueryHandler,
-  GetMyDraftsHandler,
+  GetMyDraftHandler,
   GetProfilePostsQueryHandler,
 ];
 const services = [];
