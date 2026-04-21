2548c91c33f7ace246041fbbed2a84e38da80481
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-04-21 16:22:30 +0300

    feat(subscriptions): добавить обработчик запроса на получение платежей пользователя
    - Создать класс GetMyPaymentsQuery с параметрами `userId` и `queryParams` для передачи данных запроса
    - Реализовать GetMyPaymentsQueryHandler с использованием PaymentsQueryRepository для извлечения данных о платежах пользователя
    - Добавить метод execute() для обработки запроса и возвращения пагинированного списка платежей через PaginatedViewDto
    - Обеспечить поддержку пользовательских фильтров через использование GetPaymentsQueryParams
    - Улучшить структуру кода и модульность при работе с платежами пользователей

---
+++ apps/payments/src/modules/subscriptions/application/queries/get-my-payments.query-handler.ts
@@ -0,0 +1,23 @@
+import { GetPaymentsQueryParams } from '../../api/input-dto/get-payments-query-params.input-dto';
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { PaginatedViewDto } from '../../../../common/dto/paginated.view-dto';
+import { PaymentViewDto } from '../../api/view-dto/payment.view-dto';
+import { PaymentsQueryRepository } from '../../infrastructure/query/paments.query-repository';
+
+export class GetMyPaymentsQuery {
+  constructor(
+    public readonly userId: number,
+    public readonly queryParams: GetPaymentsQueryParams,
+  ) {}
+}
+
+@QueryHandler(GetMyPaymentsQuery)
+export class GetMyPaymentsQueryHandler
+  implements IQueryHandler<GetMyPaymentsQuery, PaginatedViewDto<PaymentViewDto>>
+{
+  constructor(private readonly paymentsQueryRepository: PaymentsQueryRepository) {}
+
+  execute({ userId, queryParams }: GetMyPaymentsQuery): Promise<PaginatedViewDto<PaymentViewDto>> {
+    return this.paymentsQueryRepository.getPaymentsForUser(userId, queryParams);
+  }
+}
