1d6d47a43cc3989d4ad910aaf712086473eea53e
kunitsynvp <kunitsynvp@mail.ru>
2026-04-28 14:36:33 +0300

    refactor(payments): улучшены утилиты для работы с webhook событиями

--- apps/payments/src/modules/subscriptions/application/webhook/handlers/utils/check-is-old-event.ts
+++ apps/payments/src/modules/subscriptions/application/webhook/handlers/utils/check-is-old-event.ts
@@ -2,7 +2,5 @@ import { Subscription } from '@generated/prisma-payments';
 import Stripe from 'stripe';
 
 export function checkIsOldEvent(event: Stripe.Event, localSub: Subscription) {
-  return !(
-    localSub.lastStripeEventAt && new Date(event.created * 1000) < localSub.lastStripeEventAt
-  );
+  return localSub.lastStripeEventAt && new Date(event.created * 1000) < localSub.lastStripeEventAt;
 }
--- apps/payments/src/modules/subscriptions/application/webhook/handlers/utils/extract-subscription-id.ts
+++ apps/payments/src/modules/subscriptions/application/webhook/handlers/utils/extract-subscription-id.ts
@@ -18,3 +18,15 @@ export function extractSubscriptionId(
 
   return null;
 }
+export function extractSubscriptionIdFromCS(
+  checkoutSession: Stripe.Checkout.Session,
+): string | null {
+  const subscription = checkoutSession.subscription;
+  if (typeof subscription === 'string') {
+    return subscription;
+  }
+  if (subscription && typeof subscription === 'object') {
+    return subscription.id;
+  }
+  return null;
+}
