93a3af083e9e7dab7f91564a522f1cec42b1392a
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-05-09 14:23:22 +0300

    feat(logger): добавить LoggerFactory в модуль логгирования
    - Добавить `LoggerFactory` в секции `providers` и `exports` в `LoggerModule`
    - Обеспечить доступность `LoggerFactory` в других модулях через экспорт из `LoggerModule`
    - Повысить удобство создания контекстных логгеров на уровне приложения

--- apps/snapflow-core/src/modules/logger/logger.module.ts
+++ apps/snapflow-core/src/modules/logger/logger.module.ts
@@ -2,12 +2,13 @@ import { Global, Module } from '@nestjs/common';
 import { CustomLogger } from './logger.service';
 import { WinstonService } from './winston.service';
 import { AsyncLocalStorageService } from '../../common/async-local-storage/async-local-storage.service';
+import { LoggerFactory } from './logger.factory';
 
 @Global()
 @Module({
   imports: [],
   controllers: [],
-  providers: [CustomLogger, WinstonService, AsyncLocalStorageService],
-  exports: [CustomLogger],
+  providers: [CustomLogger, LoggerFactory, WinstonService, AsyncLocalStorageService],
+  exports: [CustomLogger, LoggerFactory, AsyncLocalStorageService],
 })
 export class LoggerModule {}
