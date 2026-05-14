9f814c255eaae3371b97db2478ab55f087fb0883
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-05-14 21:40:37 +0300

    feat(logger): добавить логгирование в GlobalExceptionsFilter
    - Интегрировать LoggerFactory в globalExceptionFilterSetup для создания filterLogger
    - Добавить интерфейс GlobalExceptionsFilterLogger для логгирования ошибок и предупреждений
    - Дополнить GlobalExceptionsFilter методом logException с поддержкой error и warn
    - Указать source метода ("GlobalExceptionsFilter.logException") для повышения структурированности логов
    - Улучшить отслеживаемость и информативность логирования исключений в приложении payments

--- apps/payments/src/setup/global-exception-filter.setup.ts
+++ apps/payments/src/setup/global-exception-filter.setup.ts
@@ -2,15 +2,33 @@ import { INestApplication } from '@nestjs/common';
 import {
   DomainHttpExceptionsFilter,
   GlobalExceptionsFilter,
+  type GlobalExceptionsFilterLogger,
   ValidationExceptionFilter,
 } from '../../../../libs/exceptions/http/filters';
 import { NotificationResultCodeType } from '../common/notification/notification-result-code';
 import { NotificationResultCodeMapper } from '../common/notification/notification-result-code.mapper';
+import { LoggerFactory } from '../modules/logger/logger.factory';
 
 export function globalExceptionFilterSetup(app: INestApplication, isExposeDetails: boolean) {
   const mapper: NotificationResultCodeMapper = app.get(NotificationResultCodeMapper);
 
-  app.useGlobalFilters(new GlobalExceptionsFilter(isExposeDetails));
+  const loggerFactory: LoggerFactory = app.get(LoggerFactory);
+  const filterLogger = loggerFactory.create(GlobalExceptionsFilter.name);
+
+  const globalFilterLogger: GlobalExceptionsFilterLogger = {
+    error: (message: string, stack?: string): void => {
+      const err = new Error(message);
+      if (stack !== undefined && stack !== '') {
+        err.stack = stack;
+      }
+      filterLogger.error(err, 'GlobalExceptionsFilter.logException');
+    },
+    warn: (message: string): void => {
+      filterLogger.warn(message, 'GlobalExceptionsFilter.logException');
+    },
+  };
+
+  app.useGlobalFilters(new GlobalExceptionsFilter(isExposeDetails, undefined, globalFilterLogger));
   app.useGlobalFilters(new DomainHttpExceptionsFilter<NotificationResultCodeType>(mapper));
   app.useGlobalFilters(new ValidationExceptionFilter());
 }
