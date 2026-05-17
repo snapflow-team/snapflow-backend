d53e457ab8c31e437814ff1a35848abc7379574c
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-05-17 16:33:26 +0300

    feat(nextjs): расширить зависимости модуля интеграции
    - Добавить HomeRevalidationCountersStore в список провайдеров NextjsIntegrationModule
    - Добавить RecordHomeRevalidationActivityUseCase в список провайдеров для обработки активности
    - Обновить конфигурацию модуля для поддержки новых компонентов
    - Подготовить модуль к использованию новых функциональностей для ревалидации

--- apps/snapflow-core/src/modules/integrations/nextjs/nextjs-integration.module.ts
+++ apps/snapflow-core/src/modules/integrations/nextjs/nextjs-integration.module.ts
@@ -3,9 +3,17 @@ import { HttpModule } from '@nestjs/axios';
 import { NextjsRevalidationService } from './nextjs-revalidation.service';
 import { RevalidateOnPostCreatedEventHandler } from './event-handlers/revalidate-on-post-created.event-handler';
 import { CryptoService } from '../../../../../../libs/common/services/crypto.service';
+import { HomeRevalidationCountersStore } from './infrastructure/home-revalidation-counters.store';
+import { RecordHomeRevalidationActivityUseCase } from './application/record-home-revalidation-activity-usecase';
 
 @Module({
   imports: [HttpModule],
-  providers: [NextjsRevalidationService, RevalidateOnPostCreatedEventHandler, CryptoService],
+  providers: [
+    NextjsRevalidationService,
+    HomeRevalidationCountersStore,
+    RecordHomeRevalidationActivityUseCase,
+    RevalidateOnPostCreatedEventHandler,
+    CryptoService,
+  ],
 })
 export class NextjsIntegrationModule {}
