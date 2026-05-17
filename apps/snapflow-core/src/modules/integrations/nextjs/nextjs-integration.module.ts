4132234fd838f33366711bc0902412e6fbc12f6e
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-05-17 16:44:10 +0300

    feat(nextjs): добавить обработчик события для ревалидации при регистрации
    - Создать класс RevalidateOnNewSignupEventHandler для обработки новых регистраций
    - Зарегистрировать RevalidateOnNewSignupEventHandler в провайдерах NextjsIntegrationModule
    - Расширить модуль интеграции для поддержки нового функционала ревалидации
    - Подготовить инфраструктуру для автоматической ревалидации главной страницы при новой регистрации

--- apps/snapflow-core/src/modules/integrations/nextjs/nextjs-integration.module.ts
+++ apps/snapflow-core/src/modules/integrations/nextjs/nextjs-integration.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { NextjsRevalidationService } from './nextjs-revalidation.service';
 import { RevalidateOnPostCreatedEventHandler } from './event-handlers/revalidate-on-post-created.event-handler';
+import { RevalidateOnNewSignupEventHandler } from './event-handlers/revalidate-on-new-signup.event-handler';
 import { CryptoService } from '../../../../../../libs/common/services/crypto.service';
 import { HomeRevalidationCountersStore } from './infrastructure/home-revalidation-counters.store';
 import { RecordHomeRevalidationActivityUseCase } from './application/record-home-revalidation-activity-usecase';
@@ -13,6 +14,7 @@ import { RecordHomeRevalidationActivityUseCase } from './application/record-home
     HomeRevalidationCountersStore,
     RecordHomeRevalidationActivityUseCase,
     RevalidateOnPostCreatedEventHandler,
+    RevalidateOnNewSignupEventHandler,
     CryptoService,
   ],
 })
