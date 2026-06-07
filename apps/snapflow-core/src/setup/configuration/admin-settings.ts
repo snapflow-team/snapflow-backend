14d0395066c6fd3ef5cfabd7939875c7b2981b2e
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-06-07 14:29:32 +0300

    refactor(admin): переименовать sessionMaxAge на sessionMaxAgeHours
    - Заменить поле sessionMaxAge на sessionMaxAgeHours в классе AdminSettings
    - Обновить использование переменной environmentVariables.ADMIN_SESSION_MAX_AGE на ADMIN_SESSION_MAX_AGE_HOURS
    - Повысить читаемость и точность обозначения единиц измерения времени настройки сессии

--- apps/snapflow-core/src/setup/configuration/admin-settings.ts
+++ apps/snapflow-core/src/setup/configuration/admin-settings.ts
@@ -9,11 +9,11 @@ export class AdminSettings {
   password: string;
 
   @IsNumber()
-  sessionMaxAge: number;
+  sessionMaxAgeHours: number;
 
   constructor(private readonly environmentVariables: EnvironmentVariable) {
     this.email = environmentVariables.ADMIN_EMAIL;
     this.password = environmentVariables.ADMIN_PASSWORD;
-    this.sessionMaxAge = Number(environmentVariables.ADMIN_SESSION_MAX_AGE);
+    this.sessionMaxAgeHours = Number(environmentVariables.ADMIN_SESSION_MAX_AGE_HOURS);
   }
 }
