766071753415db59899c16545c05be46b88c4dd8
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-06-10 23:24:55 +0300

    refactor(admin): удалить избыточные аннотации для валидации в AdminUsersQueryInput
    - Удалить @IsNumber для page и pageSize, так как они уже проверяются через @Field с типом Int
    - Удалить @IsString для search, потому что GraphQL автоматически валидирует тип String
    - Удалить @IsEnum и @IsOptional для sortBy и sortDirection, так как они валидируются через GraphQL с использованием enum и nullable
    - Уменьшить избыточность кода, полагаясь на встроенную валидацию GraphQL
    - Упростить класс AdminUsersQueryInput, сохраняя его функциональность полной

--- apps/snapflow-core/src/modules/admin/api/inputs/admin-users-query.input.ts
+++ apps/snapflow-core/src/modules/admin/api/inputs/admin-users-query.input.ts
@@ -1,5 +1,5 @@
 import { Field, InputType, Int } from '@nestjs/graphql';
-import { IsEnum, IsNumber, IsOptional, IsString, Min } from 'class-validator';
+import { IsOptional, Min } from 'class-validator';
 import { AdminSortDirection } from '../../domain/enums/admin-sort-direction.enum';
 import { AdminUsersSortField } from '../../domain/enums/admin-users-sort-field.enum';
 
@@ -7,34 +7,26 @@ import { AdminUsersSortField } from '../../domain/enums/admin-users-sort-field.e
 export class AdminUsersQueryInput {
   @Field(() => Int, { defaultValue: 1 })
   @IsOptional()
-  @IsNumber()
   @Min(1)
   page?: number;
 
   @Field(() => Int, { defaultValue: 8 })
   @IsOptional()
-  @IsNumber()
   @Min(1)
   pageSize?: number;
 
   @Field(() => String, { nullable: true })
-  @IsOptional()
-  @IsString()
   search?: string;
 
   @Field(() => AdminUsersSortField, {
     nullable: true,
     defaultValue: AdminUsersSortField.CreatedAt,
   })
-  @IsOptional()
-  @IsEnum(AdminUsersSortField)
   sortBy?: AdminUsersSortField;
 
   @Field(() => AdminSortDirection, {
     nullable: true,
     defaultValue: AdminSortDirection.Descending,
   })
-  @IsOptional()
-  @IsEnum(AdminSortDirection)
   sortDirection?: AdminSortDirection;
 }
