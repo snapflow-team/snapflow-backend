49bcce64b9e64e105dfcffa83b06cf33f62a31b3
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-06-14 20:45:25 +0300

    feat(admin): добавить новые поля в AdminSettings и их валидацию
    - Добавить поле paymentsServiceUrl с декоратором @IsUrl для проверки на корректный URL (опции: require_tld: false, protocols: ['http', 'https'])
    - Добавить поле internalApiSecret с декоратором @IsNotEmpty для обязательной проверки заполнения
    - Расширить класс AdminSettings с учетом данных из environment переменных (PAYMENTS_SERVICE_URL и INTERNAL_API_SECRET)
    - Повысить надежность валидации для новых параметров.

--- apps/snapflow-core/src/setup/configuration/admin-settings.ts
+++ apps/snapflow-core/src/setup/configuration/admin-settings.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsNumber } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsNumber, IsUrl } from 'class-validator';
 import { EnvironmentVariable } from './configuration';
 
 export class AdminSettings {
@@ -11,9 +11,17 @@ export class AdminSettings {
   @IsNumber()
   sessionMaxAgeHours: number;
 
+  @IsUrl({ require_tld: false, protocols: ['http', 'https'] })
+  paymentsServiceUrl: string;
+
+  @IsNotEmpty()
+  internalApiSecret: string;
+
   constructor(private readonly environmentVariables: EnvironmentVariable) {
     this.email = environmentVariables.ADMIN_EMAIL;
     this.password = environmentVariables.ADMIN_PASSWORD;
     this.sessionMaxAgeHours = Number(environmentVariables.ADMIN_SESSION_MAX_AGE_HOURS);
+    this.paymentsServiceUrl = environmentVariables.PAYMENTS_SERVICE_URL;
+    this.internalApiSecret = environmentVariables.INTERNAL_API_SECRET;
   }
 }
