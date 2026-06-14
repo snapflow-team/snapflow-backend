2a73ba887b65f30c6c50c54bd7aef223f6b2345e
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-06-14 13:22:27 +0300

    refactor(admin): обновить значения query defaults на объектные константы
    - Заменить отдельные переменные ADMIN_DEFAULT_PAGE, ADMIN_DEFAULT_PAGE_SIZE и другие на объекты adminUsersQueryDefaults и adminPaymentsQueryDefaults
    - Включить в adminUsersQueryDefaults поля page, pageSize, sortBy и sortDirection для упрощения управления настройками
    - Создать adminPaymentsQueryDefaults с аналогичными параметрами, но с pageSize = 6 и sortBy = AdminPaymentsSortField.Date
    - Улучшить читаемость и поддержку кода, сгруппировав связанные настройки в объекты

--- apps/snapflow-core/src/modules/admin/constants/admin-query.defaults.ts
+++ apps/snapflow-core/src/modules/admin/constants/admin-query.defaults.ts
@@ -2,8 +2,16 @@ import { AdminPaymentsSortField } from '../domain/enums/admin-payments-sort-fiel
 import { AdminSortDirection } from '../domain/enums/admin-sort-direction.enum';
 import { AdminUsersSortField } from '../domain/enums/admin-users-sort-field.enum';
 
-export const ADMIN_DEFAULT_PAGE = 1;
-export const ADMIN_DEFAULT_PAGE_SIZE = 8;
-export const ADMIN_DEFAULT_SORT_DIRECTION = AdminSortDirection.Descending;
-export const ADMIN_DEFAULT_USERS_SORT_BY = AdminUsersSortField.CreatedAt;
-export const ADMIN_DEFAULT_PAYMENTS_SORT_BY = AdminPaymentsSortField.CreatedAt;
+export const adminUsersQueryDefaults = {
+  page: 1,
+  pageSize: 8,
+  sortBy: AdminUsersSortField.CreatedAt,
+  sortDirection: AdminSortDirection.Descending,
+} as const;
+
+export const adminPaymentsQueryDefaults = {
+  page: 1,
+  pageSize: 6,
+  sortBy: AdminPaymentsSortField.Date,
+  sortDirection: AdminSortDirection.Descending,
+} as const;
