73bbd87b87b1ce650eabaf8e6cfc96a9733caecd
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-06-26 15:58:49 +0300

    feat(profile): добавить проверку подписки для публичного профиля
    - Добавить использование `FollowsQueryRepository` в `GetPublicProfileQueryHandler` для проверки подписки
    - Дополнить метод `execute` проверкой подписки через `isFollowing` при наличии `viewerId`
    - Расширить `GetPublicProfileQuery`, добавив опциональное поле `viewerId`
    - Повысить удобство использования API, позволяя отображать статус подписки в публичном профиле

--- apps/snapflow-core/src/modules/user-accounts/users/profile/application/queries/get-public-profile.query-handler.ts
+++ apps/snapflow-core/src/modules/user-accounts/users/profile/application/queries/get-public-profile.query-handler.ts
@@ -1,18 +1,37 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { ProfilesQueryRepository } from '../../infrastructure/query/profiles.query-repository';
 import { PublicProfileViewDto } from '../../api/dto/view-dto/public-profile.view-dto';
+import { FollowsQueryRepository } from '../../../../../follows/infrastructure/follows.query-repository';
 
 export class GetPublicProfileQuery {
-  constructor(public readonly profileId: number) {}
+  constructor(
+    public readonly profileId: number,
+    public readonly viewerId?: number,
+  ) {}
 }
 
 @QueryHandler(GetPublicProfileQuery)
 export class GetPublicProfileQueryHandler
   implements IQueryHandler<GetPublicProfileQuery, PublicProfileViewDto>
 {
-  constructor(private readonly profilesQueryRepository: ProfilesQueryRepository) {}
+  constructor(
+    private readonly profilesQueryRepository: ProfilesQueryRepository,
+    private readonly followsQueryRepository: FollowsQueryRepository,
+  ) {}
 
-  async execute({ profileId }: GetPublicProfileQuery): Promise<PublicProfileViewDto> {
-    return this.profilesQueryRepository.findProfileWithMetadataForUserByIdOrNotFoundFail(profileId);
+  async execute({ profileId, viewerId }: GetPublicProfileQuery): Promise<PublicProfileViewDto> {
+    const profile: PublicProfileViewDto =
+      await this.profilesQueryRepository.findProfileWithMetadataForUserByIdOrNotFoundFail(
+        profileId,
+      );
+
+    if (viewerId !== undefined) {
+      profile.isFollowing = await this.followsQueryRepository.isFollowing(
+        viewerId,
+        Number(profile.userId),
+      );
+    }
+
+    return profile;
   }
 }
