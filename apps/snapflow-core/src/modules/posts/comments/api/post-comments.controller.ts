203a971ab3b7b9870f3b53ec683abce234d2c2a5
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-06-27 19:07:03 +0300

    feat(comments): создать контроллер для работы с комментариями постов
    - Добавить класс `PostCommentsController` с методами для создания и получения комментариев
    - Реализовать метод `createComment` для добавления нового комментария к посту:
      - Использовать `CommandBus` для выполнения команды `CreateCommentCommand`
      - Вернуть созданный комментарий через `QueryBus` и запрос `GetCommentQuery`
    - Реализовать метод `getPostComments` для получения комментариев поста с пагинацией:
      - Применить query-параметры из `GetPostCommentsQueryParamsDto`
      - Выполнить запрос `GetPostCommentsQuery` через `QueryBus`
    - Добавить декораторы `JwtAuthGuard` для защиты маршрутов и `Public` для публичного получения комментариев
    - Генерировать документацию с помощью Swagger-декораторов `CreateCommentSwagger` и `

---
+++ apps/snapflow-core/src/modules/posts/comments/api/post-comments.controller.ts
@@ -0,0 +1,57 @@
+import { Body, Controller, Get, Param, ParseIntPipe, Post, Query, UseGuards } from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { ExtractUserFromRequest } from '../../../user-accounts/auth/domain/guards/decorators/extract-user-from-request.decorator';
+import { UserContextDto } from '../../../user-accounts/auth/domain/guards/dto/user-context.dto';
+import { JwtAuthGuard } from '../../../user-accounts/auth/domain/guards/bearer/jwt-auth.guard';
+import { Public } from '../../../user-accounts/decorators/public.decorator';
+import { CreateCommentInputDto } from './input-dto/create-comment.input-dto';
+import { GetPostCommentsQueryParamsDto } from './input-dto/get-post-comments.query-params.dto';
+import { CommentItemViewDto } from './view-dto/comment-item.view-dto';
+import { PostCommentsPageViewDto } from './view-dto/post-comments-page.view-dto';
+import { CreateCommentCommand } from '../application/usecases/create-comment.usecase';
+import { GetCommentQuery } from '../application/queries/get-comment.query-handler';
+import { GetPostCommentsQuery } from '../application/queries/get-post-comments.query-handler';
+import { CreateCommentSwagger } from './swagger/create-comment.swagger';
+import { GetPostCommentsSwagger } from './swagger/get-post-comments.swagger';
+
+@Controller('posts')
+@UseGuards(JwtAuthGuard)
+export class PostCommentsController {
+  constructor(
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
+  ) {}
+
+  @Post(':postId/comments')
+  @CreateCommentSwagger()
+  async createComment(
+    @Param('postId', ParseIntPipe) postId: number,
+    @Body() { text, parentId }: CreateCommentInputDto,
+    @ExtractUserFromRequest() { id: userId }: UserContextDto,
+  ): Promise<CommentItemViewDto> {
+    const commentId: number = await this.commandBus.execute<CreateCommentCommand, number>(
+      new CreateCommentCommand({
+        userId,
+        postId,
+        text,
+        parentId: parentId != null ? Number(parentId) : null,
+      }),
+    );
+
+    return this.queryBus.execute<GetCommentQuery, CommentItemViewDto>(
+      new GetCommentQuery(commentId),
+    );
+  }
+
+  @Get(':postId/comments')
+  @Public()
+  @GetPostCommentsSwagger()
+  async getPostComments(
+    @Param('postId', ParseIntPipe) postId: number,
+    @Query() query: GetPostCommentsQueryParamsDto,
+  ): Promise<PostCommentsPageViewDto> {
+    return this.queryBus.execute<GetPostCommentsQuery, PostCommentsPageViewDto>(
+      new GetPostCommentsQuery(query, postId),
+    );
+  }
+}
