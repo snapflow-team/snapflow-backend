396200e5b27a230334e75e044a2d8ded766719b6
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-06-27 18:47:06 +0300

    feat(comments): добавить тип для комментариев с метаданными пользователя
    - Создать объект `commentWithUserMetadataInclude` для определения полей `user` и `_count`:
      - `user`: данные пользователя (id, username, avatarUrl), исключая удалённые профили
      - `_count`: количество ответов на комментарий, исключая удалённые
    - Определить новый тип `CommentWithUserMetadata` через `Prisma.CommentGetPayload`, используя `commentWithUserMetadataInclude`
    - Повысить стандартизацию и читаемость запросов, облегчив работу с данными о комментарии и пользователе

---
+++ apps/snapflow-core/src/modules/posts/comments/infrastructure/types/comment-with-user-metadata.type.ts
@@ -0,0 +1,24 @@
+import { Prisma } from '@generated/prisma-snapflow';
+
+export const commentWithUserMetadataInclude = {
+  user: {
+    select: {
+      id: true,
+      username: true,
+      profiles: {
+        where: { deletedAt: null },
+        select: { avatarUrl: true },
+        take: 1,
+      },
+    },
+  },
+  _count: {
+    select: {
+      replies: { where: { deletedAt: null } },
+    },
+  },
+} satisfies Prisma.CommentInclude;
+
+export type CommentWithUserMetadata = Prisma.CommentGetPayload<{
+  include: typeof commentWithUserMetadataInclude;
+}>;
