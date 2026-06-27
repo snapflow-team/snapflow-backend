a70e305a3d7bf182738cbf148662f14fa7d610b9
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-06-27 19:05:29 +0300

    feat(comments): реализовать обработчик запроса получения комментариев поста
    - Добавить класс `GetPostCommentsQuery` с параметрами `query` (пагинация) и `postId`
    - Реализовать обработчик запроса `GetPostCommentsQueryHandler` с методом `execute`
    - Внедрить `PostsRepository` для проверки существования поста по ID
    - Выбрасывать `NotFoundException`, если пост не найден
    - Внедрить `CommentsQueryRepository` для получения комментариев поста с поддержкой пагинации
    - Возвращать данные в формате `PostCommentsPageViewDto` для стандартизации ответа в клиентском API

---
+++ apps/snapflow-core/src/modules/posts/comments/application/queries/get-post-comments.query-handler.ts
@@ -0,0 +1,32 @@
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { NotFoundException } from '../../../../../common/exceptions/domain-exceptions';
+import { PostsRepository } from '../../../infrastructure/posts-repository';
+import { CommentsQueryRepository } from '../../infrastructure/comments.query-repository';
+import { GetPostCommentsQueryParamsDto } from '../../api/input-dto/get-post-comments.query-params.dto';
+import { PostCommentsPageViewDto } from '../../api/view-dto/post-comments-page.view-dto';
+import { Post } from '@generated/prisma-snapflow';
+
+export class GetPostCommentsQuery {
+  constructor(
+    public readonly query: GetPostCommentsQueryParamsDto,
+    public readonly postId: number,
+  ) {}
+}
+
+@QueryHandler(GetPostCommentsQuery)
+export class GetPostCommentsQueryHandler implements IQueryHandler<GetPostCommentsQuery> {
+  constructor(
+    private readonly postsRepository: PostsRepository,
+    private readonly commentsQueryRepository: CommentsQueryRepository,
+  ) {}
+
+  async execute({ query, postId }: GetPostCommentsQuery): Promise<PostCommentsPageViewDto> {
+    const post: Post | null = await this.postsRepository.findById(postId);
+
+    if (!post) {
+      throw new NotFoundException('Post not found');
+    }
+
+    return this.commentsQueryRepository.findPostComments(postId, query);
+  }
+}
