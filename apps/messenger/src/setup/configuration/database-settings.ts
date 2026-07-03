44feb6482f648894c9ed757403d599bea5ff7912
kunitsynvp <kunitsynvp@mail.ru>
2026-07-03 15:21:41 +0300

    fix(messenger): исправить импорт Prisma  в database-settings.ts

--- apps/messenger/src/setup/configuration/database-settings.ts
+++ apps/messenger/src/setup/configuration/database-settings.ts
@@ -1,6 +1,6 @@
 import { IsBoolean, IsString } from 'class-validator';
 import { EnvironmentVariable } from './configuration';
-import { Prisma } from '@generated/prisma-payments';
+import { Prisma } from '@generated/prisma-messenger';
 
 export class DatabaseSettings {
   @IsString()
