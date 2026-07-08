1a2e655b1ddf6ca345b544789313ef114f7e9206
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-07-08 23:32:08 +0300

    feat(messaging): добавить WebSocket провайдеры в MessagingModule
    - Подключить MessengerWebSocketGateway и MessengerWebSocketService в MessagingModule через массив websocketProviders
    - Добавить websocketProviders в providers модуля для обеспечения WebSocket функциональности
    - Повысить возможность расширения модуля для работы с WebSocket соединениями

--- apps/messenger/src/modules/messaging/messaging.module.ts
+++ apps/messenger/src/modules/messaging/messaging.module.ts
@@ -4,13 +4,16 @@ import { MessagingController } from './api/messaging.controller';
 import { SendMessageUseCase } from './application/usecases/send-message.usecase';
 import { ChatsRepository } from './infrastructure/chats.repository';
 import { MessagesRepository } from './infrastructure/messages.repository';
+import { MessengerWebSocketGateway } from './websocket/gateway/messenger-websocket.gateway';
+import { MessengerWebSocketService } from './websocket/services/messenger-websocket.service';
 
 const useCases = [SendMessageUseCase];
 const repositories = [ChatsRepository, MessagesRepository];
 const guards = [RemoteAuthGuard];
+const websocketProviders = [MessengerWebSocketGateway, MessengerWebSocketService];
 
 @Module({
   controllers: [MessagingController],
-  providers: [...useCases, ...repositories, ...guards],
+  providers: [...useCases, ...repositories, ...guards, ...websocketProviders],
 })
 export class MessagingModule {}
