8aa60634bed93d8cae3c97de84f2d5ec4d140752
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-07-12 19:48:35 +0300

    refactor(messaging): удалить устаревшие типы и обновить зависимости модулей
    - Удалить типы UserChatListItem и FindUserChatsPaginatedParams из types/user-chat-list-item.type.ts как больше не используемые
    - Добавить ChatsQueryRepository в repositories массива MessagingModule для поддержки новых запросов
    - Упростить кодовую базу, убрав неактуальные зависимости и подготовив модуль к дальнейшему улучшению работы с репозиториями

--- apps/messenger/src/modules/messaging/messaging.module.ts
+++ apps/messenger/src/modules/messaging/messaging.module.ts
@@ -6,6 +6,7 @@ import { GetChatMessagesQueryHandler } from './application/queries/get-chat-mess
 import { GetUserChatsQueryHandler } from './application/queries/get-user-chats.query-handler';
 import { GetOrCreateChatUseCase } from './application/usecases/get-or-create-chat.usecase';
 import { SendMessageUseCase } from './application/usecases/send-message.usecase';
+import { ChatsQueryRepository } from './infrastructure/query/chats.query-repository';
 import { ChatsRepository } from './infrastructure/chats.repository';
 import { MessagesRepository } from './infrastructure/messages.repository';
 import { MessengerWebSocketGateway } from './websocket/gateway/messenger-websocket.gateway';
@@ -13,7 +14,7 @@ import { MessengerWebSocketService } from './websocket/services/messenger-websoc
 
 const useCases = [SendMessageUseCase, GetOrCreateChatUseCase];
 const queries = [GetUserChatsQueryHandler, GetChatMessagesQueryHandler];
-const repositories = [ChatsRepository, MessagesRepository];
+const repositories = [ChatsRepository, ChatsQueryRepository, MessagesRepository];
 const guards = [AccessTokenGuard, ChatMembershipGuard];
 const websocketProviders = [MessengerWebSocketGateway, MessengerWebSocketService];
 
