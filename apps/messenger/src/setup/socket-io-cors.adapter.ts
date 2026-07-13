d4af251ff01ec790e96fb99f0ee10cec5b17176b
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-07-13 22:44:38 +0300

    feat(messenger): добавить поддержку Redis Adapter в SocketIoCorsAdapter
    - Подключить Redis Adapter через библиотеку @socket.io/redis-adapter для масштабирования WebSocket
    - Добавить метод connectToRedis для подключения клиентов Redis и подсоединений
    - Изменить createIOServer: установить Redis Adapter, если он доступен, для поддержки кластеризации
    - Добавить ContextLogger для логирования в SocketIoCorsAdapter
    - Повысить стабильность и масштабируемость WebSocket соединений через Redis

--- apps/messenger/src/setup/socket-io-cors.adapter.ts
+++ apps/messenger/src/setup/socket-io-cors.adapter.ts
@@ -1,23 +1,50 @@
 import { INestApplicationContext } from '@nestjs/common';
 import { IoAdapter } from '@nestjs/platform-socket.io';
-import { ServerOptions } from 'socket.io';
+import { createAdapter } from '@socket.io/redis-adapter';
+import { Redis } from 'ioredis';
+import { Server, ServerOptions } from 'socket.io';
+import { LoggerFactory } from '../modules/logger/logger.factory';
+import { ContextLogger } from '../modules/logger/context-logger';
 
 export class SocketIoCorsAdapter extends IoAdapter {
+  private adapterConstructor?: ReturnType<typeof createAdapter>;
+  private readonly logger: ContextLogger;
+
   constructor(
     app: INestApplicationContext,
     private readonly origin: string[] | boolean,
   ) {
     super(app);
+    this.logger = app.get(LoggerFactory).create(SocketIoCorsAdapter.name);
   }
 
-  createIOServer(port: number, options?: ServerOptions): unknown {
-    return super.createIOServer(port, {
+  async connectToRedis(redisClient: Redis): Promise<void> {
+    const subClient: Redis = redisClient.duplicate();
+
+    try {
+      await Promise.all([redisClient.connect(), subClient.connect()]);
+    } catch (error) {
+      this.logger.error(error, this.connectToRedis.name);
+      throw error;
+    }
+
+    this.adapterConstructor = createAdapter(redisClient, subClient);
+  }
+
+  createIOServer(port: number, options?: ServerOptions): Server {
+    const server: Server = super.createIOServer(port, {
       ...options,
       cors: {
         ...options?.cors,
         origin: this.origin,
         credentials: true,
       },
-    });
+    }) as Server;
+
+    if (this.adapterConstructor) {
+      server.adapter(this.adapterConstructor);
+    }
+
+    return server;
   }
 }
