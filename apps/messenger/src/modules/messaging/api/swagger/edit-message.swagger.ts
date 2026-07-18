47f5116369bd38096a08aabd7dd0cfeff3c0aa6f
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-07-18 20:08:09 +0300

    feat(messaging): добавить swagger-декоратор для редактирования сообщения
    - Создать EditMessageSwagger для описания эндпоинта редактирования сообщения в Swagger
    - Добавить ApiOperation с описанием: только автор может редактировать сообщение в определённый интервал времени
    - Указать параметры декоратора: messageId (тип Number), тело запроса (тип EditMessageInputDto)
    - Описать успешный ответ типа MessageViewDto и возможные ошибки: 400, 401, 403, 404
    - Повысить документированность API: чёткое описание ограничений и возможных ошибок для данного эндпоинта

---
+++ apps/messenger/src/modules/messaging/api/swagger/edit-message.swagger.ts
@@ -0,0 +1,49 @@
+import { applyDecorators } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiBody,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { EditMessageInputDto } from '../input-dto/edit-message.input-dto';
+import { MessageViewDto } from '../view-dto/message.view-dto';
+
+export function EditMessageSwagger() {
+  return applyDecorators(
+    ApiBearerAuth('access-token'),
+    ApiOperation({
+      summary: 'Редактировать текст сообщения',
+      description:
+        'Только автор может редактировать сообщение в пределах окна редактирования. ' +
+        'Удалённые для всех сообщения редактировать нельзя.',
+    }),
+    ApiParam({
+      name: 'messageId',
+      type: Number,
+      description: 'Идентификатор сообщения',
+      example: 100,
+    }),
+    ApiBody({ type: EditMessageInputDto }),
+    ApiOkResponse({
+      description: 'Сообщение успешно отредактировано',
+      type: MessageViewDto,
+    }),
+    ApiBadRequestResponse({
+      description: 'Невалидный текст сообщения или messageId',
+    }),
+    ApiUnauthorizedResponse({ description: 'Пользователь не авторизован' }),
+    ApiForbiddenResponse({
+      description:
+        'Пользователь не участник чата, не автор сообщения, окно редактирования истекло ' +
+        'или сообщение удалено для всех',
+    }),
+    ApiNotFoundResponse({
+      description: 'Сообщение не найдено',
+    }),
+  );
+}
