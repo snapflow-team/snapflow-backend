dce93d5d33a045d25137fa3f206a660484ed050d
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-07-23 21:27:51 +0300

    feat(messaging): добавить эндпоинты для статуса активности и присутствия
    - Добавить метод getPresence в MessagingController для получения статусов присутствия пользователей
    - Добавить метод updateActivityStatus в MessagingController для управления видимостью активности
    - Интегрировать DTO: GetPresenceQueryParamsDto, UpdateActivityStatusInputDto, PresenceViewDto
    - Настроить документацию через Swagger: UpdateActivityStatusSwagger, GetPresenceSwagger
    - Обеспечить типизацию и валидацию для входных данных, включая userIds и showActivityStatus
    - Улучшить API для управления статусом активности и получения текущей видимости участников

--- apps/messenger/src/modules/messaging/api/messaging.controller.ts
+++ apps/messenger/src/modules/messaging/api/messaging.controller.ts
@@ -19,7 +19,9 @@ import { UserContextDto } from '../../auth/guards/dto/user-context.dto';
 import { AccessTokenGuard } from '../../auth/guards/access-token.guard';
 import { DeleteMessageCommand } from '../application/commands/delete-message.command';
 import { EditMessageCommand } from '../application/commands/edit-message.command';
+import { UpdateActivityStatusCommand } from '../application/commands/update-activity-status.command';
 import { GetChatMessagesQuery } from '../application/queries/get-chat-messages.query-handler';
+import { GetPresenceQuery } from '../application/queries/get-presence.query-handler';
 import { GetUserChatsQuery } from '../application/queries/get-user-chats.query-handler';
 import { GetOrCreateChatCommand } from '../application/usecases/get-or-create-chat.usecase';
 import { MarkChatReadCommand } from '../application/usecases/mark-chat-read.usecase';
@@ -29,19 +31,24 @@ import { DeleteMessageQueryDto } from './input-dto/delete-message.query-dto';
 import { EditMessageInputDto } from './input-dto/edit-message.input-dto';
 import { GetChatMessagesQueryParamsDto } from './input-dto/get-chat-messages.query-params.dto';
 import { GetOrCreateChatInputDto } from './input-dto/get-or-create-chat.input-dto';
+import { GetPresenceQueryParamsDto } from './input-dto/get-presence.query-params.dto';
 import { GetUserChatsQueryParamsDto } from './input-dto/get-user-chats.query-params.dto';
 import { MarkChatReadInputDto } from './input-dto/mark-chat-read.input-dto';
 import { SendMessageInputDto } from './input-dto/send-message.input-dto';
+import { UpdateActivityStatusInputDto } from './input-dto/update-activity-status.input-dto';
 import { DeleteMessageSwagger } from './swagger/delete-message.swagger';
 import { EditMessageSwagger } from './swagger/edit-message.swagger';
 import { GetChatMessagesSwagger } from './swagger/get-chat-messages.swagger';
 import { GetOrCreateChatSwagger } from './swagger/get-or-create-chat.swagger';
+import { GetPresenceSwagger } from './swagger/get-presence.swagger';
 import { GetUserChatsSwagger } from './swagger/get-user-chats.swagger';
 import { MarkChatReadSwagger } from './swagger/mark-chat-read.swagger';
 import { SendMessageSwagger } from './swagger/send-message.swagger';
+import { UpdateActivityStatusSwagger } from './swagger/update-activity-status.swagger';
 import { ChatMessagesPageViewDto } from './view-dto/chat-messages-page.view-dto';
 import { ChatViewDto } from './view-dto/chat.view-dto';
 import { MessageViewDto } from './view-dto/message.view-dto';
+import { PresenceViewDto } from './view-dto/presence.view-dto';
 import { UserChatsPageViewDto } from './view-dto/user-chats-page.view-dto';
 
 @ApiTags('Messenger')
@@ -52,6 +59,27 @@ export class MessagingController {
     private readonly commandBus: CommandBus,
     private readonly queryBus: QueryBus,
   ) {}
+
+  @Get('presence')
+  @GetPresenceSwagger()
+  async getPresence(
+    @Query() query: GetPresenceQueryParamsDto,
+    @ExtractUserFromRequest() { id: userId }: UserContextDto,
+  ): Promise<PresenceViewDto[]> {
+    return this.queryBus.execute<GetPresenceQuery, PresenceViewDto[]>(
+      new GetPresenceQuery(userId, query.userIds),
+    );
+  }
+
+  @Patch('settings/activity-status')
+  @UpdateActivityStatusSwagger()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async updateActivityStatus(
+    @Body() { showActivityStatus }: UpdateActivityStatusInputDto,
+    @ExtractUserFromRequest() { id: userId }: UserContextDto,
+  ): Promise<void> {
+    await this.commandBus.execute(new UpdateActivityStatusCommand({ userId, showActivityStatus }));
+  }
 
   @Get('chats')
   @GetUserChatsSwagger()
