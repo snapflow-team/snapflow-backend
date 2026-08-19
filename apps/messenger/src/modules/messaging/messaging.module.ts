372107c7248e3f59b14b7ea42ed4ad5cb5f5e8c8
Vilyam Zautsev <z.v.m.365@gmail.com>
2026-08-19 22:27:34 +0300

    feat(messaging): подключить RabbitMQModule и оптимизировать dependencies
    - Импортировать RabbitMQModule в MessagingModule для интеграции с RabbitMQ
    - Добавить NewMessageNotificationPolicy и NewMessageNotificationDispatcherService в notificationServices
    - Зарегистрировать ChatMuteRepository в providers для обработки mute-настроек
    - Улучшить модульность и масштабируемость обработки событий и уведомлений
    - Обеспечить доступность необходимых зависимостей для расширения функциональности уведомлений

--- apps/messenger/src/modules/messaging/messaging.module.ts
+++ apps/messenger/src/modules/messaging/messaging.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AccessTokenGuard } from '../auth/guards/access-token.guard';
 import { OutboxModule } from '../outbox/outbox.module';
+import { RabbitMQModule } from '../rabbitmq/rabbitmq.module';
 import { ChatMembershipGuard } from './api/guards/chat-membership.guard';
 import { MessagingController } from './api/messaging.controller';
 import { PresenceBroadcastHelper } from './application/helpers/presence-broadcast.helper';
 import { GetChatMessagesQueryHandler } from './application/queries/get-chat-messages.query-handler';
 import { GetPresenceQueryHandler } from './application/queries/get-presence.query-handler';
 import { GetUserChatsQueryHandler } from './application/queries/get-user-chats.query-handler';
+import { NewMessageNotificationDispatcherService } from './application/services/new-message-notification-dispatcher.service';
+import { NewMessageNotificationPolicy } from './application/services/new-message-notification.policy';
 import { GetOrCreateChatUseCase } from './application/usecases/get-or-create-chat.usecase';
 import { DeleteMessageUseCase } from './application/usecases/delete-message.usecase';
 import { EditMessageUseCase } from './application/usecases/edit-message.usecase';
@@ -19,6 +22,7 @@ import { SendMessageUseCase } from './application/usecases/send-message.usecase'
 import { TypingStartUseCase } from './application/usecases/typing-start.usecase';
 import { TypingStopUseCase } from './application/usecases/typing-stop.usecase';
 import { UpdateActivityStatusUseCase } from './application/usecases/update-activity-status.usecase';
+import { ChatMuteRepository } from './infrastructure/chat-mute.repository';
 import { ChatsQueryRepository } from './infrastructure/query/chats.query-repository';
 import { MessagesQueryRepository } from './infrastructure/query/messages.query-repository';
 import { ChatsRepository } from './infrastructure/chats.repository';
@@ -50,18 +54,24 @@ const repositories = [
   MessagesQueryRepository,
   PresenceRedisRepository,
   PresenceRepository,
+  ChatMuteRepository,
+];
+const notificationServices = [
+  NewMessageNotificationPolicy,
+  NewMessageNotificationDispatcherService,
 ];
 const guards = [AccessTokenGuard, ChatMembershipGuard];
 const websocketProviders = [MessengerWebSocketGateway, MessengerWebSocketService];
 const presenceHelpers = [PresenceBroadcastHelper];
 
 @Module({
-  imports: [OutboxModule],
+  imports: [OutboxModule, RabbitMQModule],
   controllers: [MessagingController],
   providers: [
     ...useCases,
     ...queries,
     ...repositories,
+    ...notificationServices,
     ...guards,
     ...websocketProviders,
     ...presenceHelpers,
